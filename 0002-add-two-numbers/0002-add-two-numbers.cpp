cf396f6961a011b336385b2c21fb52c7753b4016
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2024-12-11 12:40:30 +0530

    Time: 1 ms (44.61%), Space: 77 MB (91.9%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -10,27 +10,43 @@
  */
 class Solution {
 public:
-ListNode*iterative(ListNode*l1, ListNode*l2){
-    ListNode*ans = new ListNode(-1);
-    ListNode*it=ans;
-    int carry=0;
-    while(l1 || l2 || carry ){
-        int a = l1 ? l1->val:0;
+//ITERATIVE METHOD.
+// ListNode*iterative(ListNode*l1, ListNode*l2){
+//     ListNode*ans = new ListNode(-1);
+//     ListNode*it=ans;
+//     int carry=0;
+//     while(l1 || l2 || carry ){
+//         int a = l1 ? l1->val:0;
+//         int b = l2 ? l2->val:0;
+//         int sum = a+b+carry;
+//         int digit= sum%10;
+//          carry = sum/10;
+//         it->next =new ListNode(digit);
+//         it=it->next;
+//         l1= l1 ? l1->next:nullptr;
+//         l2= l2 ? l2->next:nullptr;
+//     }
+
+//     ListNode*finalans = ans->next;
+//     delete ans;
+//     return finalans;
+// }
+//RECURSIVE APPROACH
+ListNode*recursive(ListNode*l1,ListNode*l2 ,int carry=0){
+    if(!l1 && !l2 && !carry) return 0;
+     int a = l1 ? l1->val:0;
         int b = l2 ? l2->val:0;
         int sum = a+b+carry;
         int digit= sum%10;
          carry = sum/10;
-        it->next =new ListNode(digit);
-        it=it->next;
-        l1= l1 ? l1->next:nullptr;
-        l2= l2 ? l2->next:nullptr;
-    }
+         ListNode*ans = new ListNode(digit);
+         ans->next =recursive(l1?l1->next:l1 ,l2?l2->next:l2 ,carry);
+         return ans;
 
-    ListNode*finalans = ans->next;
-    delete ans;
-    return finalans;
+
 }
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-       return iterative(l1,l2);
+    //    return iterative(l1,l2);
+    return recursive(l1,l2);
     }
 };
