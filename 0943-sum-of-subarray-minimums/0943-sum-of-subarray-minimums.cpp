f6d5eef3181071eb9776833c086f51c144ea4e72
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2024-12-19 18:19:20 +0530

    Time: 28 ms (25.39%), Space: 47.3 MB (25.39%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+
+vector<int>nextsmallelement(vector<int>&v){
+    stack<int>s;
+    s.push(-1);
+    vector<int>ans(v.size(),-1);
+    for(int i = v.size()-1; i>=0 ;i--){
+        while(s.top()!= -1  && v[s.top()]> v[i]) s.pop();
+        ans[i]= s.top();
+        s.push(i);
+    }
+    return ans;
+}
+vector<int>prevsmallelement(vector<int>&v){
+    stack<int>s;
+    s.push(-1);
+    vector<int>ans(v.size(),-1);
+    for(int i = 0; i<v.size() ;i++){
+        while(s.top()!= -1 && v[s.top()]>= v[i]) s.pop();
+        ans[i]= s.top();
+        s.push(i);
+    }
+    return ans;
+}
+    int sumSubarrayMins(vector<int>& arr) {
+        auto next = nextsmallelement(arr);
+        auto prev = prevsmallelement(arr);
+        int sum = 0;
+        const  int mod = 1e9 + 7 ;
+
+        for(int i=0; i< arr.size() ;i++){
+            int nexti = next[i]==-1 ? next.size() : next[i] ;
+            int previ = prev[i];
+
+            int left = i - previ;
+            int right = nexti - i;
+
+            long long number_of_times = (left % mod * right % mod) % mod ;
+            long long total = (number_of_times * arr[i]) % mod ;
+            sum = (sum + total) % mod ;
+        }
+        return sum ;
+    }
+};
