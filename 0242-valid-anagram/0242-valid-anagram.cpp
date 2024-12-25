a2f33be65513ac54074dc32725f250e58103ab5c
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2024-12-25 11:32:51 +0530

    Time: 8 ms (13.51%), Space: 9.8 MB (32.94%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int>freq1;
+        unordered_map<char,int>freq2;
+        for(int i=0 ;i< s.length(); i++){
+            char ch = s[i];
+            freq1[ch]++;
+        }
+        for(int j =0 ;j< t.length() ;j++){
+            char ch = t[j];
+            freq2[ch]++;
+        }
+         for(int j =0 ;j< t.length() ;j++){
+            char ch = t[j];
+        if(freq2[ch]>freq1[ch] || s.length()> t.length()){
+           return false;
+        }
+        }
+        
+        return true;
+    }
+};
