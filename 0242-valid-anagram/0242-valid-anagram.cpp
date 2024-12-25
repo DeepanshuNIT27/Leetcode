3e0cfca7ccca5ffca5d48a62c5b0ad5b97b75514
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2024-12-25 11:44:03 +0530

    Time: 6 ms (37.41%), Space: 9.6 MB (63.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int>freq1;
-        unordered_map<char,int>freq2;
-        for(int i=0 ;i< s.length(); i++){
-            char ch = s[i];
-            freq1[ch]++;
+        // SORTING METHOD 1
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s==t){
+            return true;
         }
-        for(int j =0 ;j< t.length() ;j++){
-            char ch = t[j];
-            freq2[ch]++;
+        else{
+            return false;
         }
-         for(int j =0 ;j< t.length() ;j++){
-            char ch = t[j];
-        if(freq2[ch]>freq1[ch] || s.length()> t.length()){
-           return false;
-        }
-        }
+
         
-        return true;
+        // unordered_map<char,int>freq1;
+        // unordered_map<char,int>freq2;
+        // for(int i=0 ;i< s.length(); i++){
+        //     char ch = s[i];
+        //     freq1[ch]++;
+        // }
+        // for(int j =0 ;j< t.length() ;j++){
+        //     char ch = t[j];
+        //     freq2[ch]++;
+        // }
+        //  for(int j =0 ;j< t.length() ;j++){
+        //     char ch = t[j];
+        // if(freq2[ch]>freq1[ch] || s.length()> t.length()){
+        //    return false;
+        // }
+        // }
+        
+        // return true;
     }
 };
