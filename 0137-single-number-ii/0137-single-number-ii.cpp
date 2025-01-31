6dc39f399cad72175bfac8ff8134202a18eb7941
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-01-31 21:43:51 +0530

    Time: 0 ms (100%), Space: 14.1 MB (11.69%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>map;
+        for(int i=0 ;i<nums.size();i++){
+            map[nums[i]]++;
+        }
+        for(auto it =map.begin();it!=map.end();it++){
+            if(it->second==1){
+                return it->first;
+                break;
+            }
+        }
+        return -1;
+    }
+};
