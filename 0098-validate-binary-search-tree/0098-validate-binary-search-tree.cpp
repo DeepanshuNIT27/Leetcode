08a4d66f1720d3386fe7dd6e698f6b3d98baebc6
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-02-28 16:10:26 +0530

    Time: 0 ms (100%), Space: 22 MB (47.99%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,38 +9,57 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
- class Info{
-public:
- long long minval;
- long long maxval;
- bool isBst;
- };
-class Solution {
-public:
-Info solve(TreeNode* root,bool &ans){
-    if(root== NULL){
-        Info temp;
-        temp.minval = LONG_MAX;
-        temp.maxval = LONG_MIN;
-        temp.isBst = true;
-        return temp;
-    }
-    Info leftans = solve(root->left,ans);
-    Info rightans = solve(root->right,ans);
-    Info currans;
-     currans.minval = min((long long)leftans.minval,(long long)root->val);
-    currans.maxval = max((long long)rightans.maxval,(long long)root->val);
-   currans.isBst = (root->val> leftans.maxval && root->val < rightans.minval && leftans.isBst && rightans.isBst);
-   if(!currans.isBst){
-    ans = false;
-   }
-   return currans;
+//METHOD-1 ; LAMBA METHOD
+//  class Info{
+// public:
+//  long long minval;
+//  long long maxval;
+//  bool isBst;
+//  };
+// class Solution {
+// public:
+// Info solve(TreeNode* root,bool &ans){
+//     if(root== NULL){
+//         Info temp;
+//         temp.minval = LONG_MAX;
+//         temp.maxval = LONG_MIN;
+//         temp.isBst = true;
+//         return temp;
+//     }
+//     Info leftans = solve(root->left,ans);
+//     Info rightans = solve(root->right,ans);
+//     Info currans;
+//      currans.minval = min((long long)leftans.minval,(long long)root->val);
+//     currans.maxval = max((long long)rightans.maxval,(long long)root->val);
+//    currans.isBst = (root->val> leftans.maxval && root->val < rightans.minval && leftans.isBst && rightans.isBst);
+//    if(!currans.isBst){
+//     ans = false;
+//    }
+//    return currans;
 
-}
-    bool isValidBST(TreeNode* root) {
-        bool ans = true;
-        solve(root,ans);
-        return ans;
+// }
+//     bool isValidBST(TreeNode* root) {
+//         bool ans = true;
+//         solve(root,ans);
+//         return ans;
         
+//     }
+// };
+// METHOD-2 RANGE CHECK METHOD;
+class Solution{
+    public:
+    bool validate(TreeNode* root ,long long lb , long long ub){
+        if(!root) return true;
+
+        bool iscurrnodeinrange = root->val > lb && root->val <ub;
+
+        bool isleftbst = validate(root->left ,lb , root->val);
+        bool isrightbst = validate(root->right ,root->val ,ub);
+        return iscurrnodeinrange &&isleftbst && isrightbst;
+    }
+    bool isValidBST(TreeNode* root){
+        long long lb = LONG_MIN;
+        long long ub = LONG_MAX;
+        return validate(root,lb,ub);
     }
 };
