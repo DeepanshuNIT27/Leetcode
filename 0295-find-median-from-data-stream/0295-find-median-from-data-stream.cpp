0b8af3b77e5aa990eb1ef5faf839face4ebe17d9
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-03-12 18:19:01 +0530

    Time: 47 ms (83.98%), Space: 124.9 MB (75.01%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -3,68 +3,34 @@ public:
 priority_queue<int>maxheap;
 priority_queue<int,vector<int>,greater<int>>minheap;
     MedianFinder() {
-        
     }
     
     void addNum(int num) {
-        if(maxheap.size() == minheap.size()){
-            if(num > findMedian()){
-                // right me dalo
-                minheap.push(num);
-            }
-            else{
-                //left me dalo
-                maxheap.push(num);
-            }
-        }
-        else if( maxheap.size() == minheap.size()+1){
-            if(num > findMedian()){
-                //right me dalo
-                minheap.push(num);
-            }
-            else{
-                //left me dalo
-                int element = maxheap.top();
-                maxheap.pop();
-                minheap.push(element);
-                maxheap.push(num);
-            }
-        }
-        else if(minheap.size() == maxheap.size()+1){
-            if(num > findMedian()){
-           //right me dalo;
-            int element = minheap.top();
-                minheap.pop();
-                maxheap.push(element);
-                minheap.push(num);
-
-            }
-            else{
-            //left me dalo;
-            maxheap.push(num);
-            }
-        }
-        
+       if(maxheap.size()==0 || num< maxheap.top()){
+        maxheap.push(num);
+       }
+       else {
+        minheap.push(num);
+       }
+       if(maxheap.size()> minheap.size()+1){
+        minheap.push(maxheap.top());
+        maxheap.pop();
+       }
+       else if(minheap.size()> maxheap.size()){
+        maxheap.push(minheap.top());
+        minheap.pop();
+       }
     }
     
     double findMedian() {
-        if(maxheap.size()==0 && minheap.size()==0){
-            return 0;
-        }
-        else if(maxheap.size() == minheap.size()){
-            return (maxheap.top()+ minheap.top())/2.0;
-        }
-        else if(maxheap.size() == minheap.size()+1){
-            return maxheap.top();
-        }
-        else if(minheap.size() == maxheap.size()+1){
-            return minheap.top();
+        if(maxheap.size()== minheap.size()){
+            return (maxheap.top()+minheap.top())/2.0;
         }
         else{
-            return -1;
+            return maxheap.top();
         }
-        
     }
+      
 };
 
 /**
