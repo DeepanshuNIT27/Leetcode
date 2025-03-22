59f909dbded1db699e630b70149d05053130679d
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-03-22 18:38:41 +0530

    Time: 20 ms (5.36%), Space: 20.1 MB (6.21%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,19 +1,59 @@
+class TrieNode{
+ public:
+ char data;
+ unordered_map<char,TrieNode*>children;
+ bool isTerminal;
+ TrieNode(char value){
+    data = value;
+    isTerminal = false;
+ }
+  void insertWord(TrieNode* root , string word){
+         if(word.length()==0){
+            root->isTerminal = true;
+            return ;
+     }
+        char ch = word[0];
+        TrieNode* child;
+        if(root->children.find(ch)!=root->children.end()){
+            child = root->children[ch];
+        }
+        else{
+            child = new TrieNode(ch);
+            root->children[ch] = child;
+        }
+        insertWord(child,word.substr(1));
+    }
+    void search (TrieNode* root , string word, int &count){
+        if(word.size()==0){
+            return ;
+        }
+        char ch = word[0];
+        TrieNode* child;
+        if(root->children.find(ch)!=root->children.end()){
+            child = root->children[ch];
+            count++;
+        }
+        else{
+            return ;
+        }
+        search(child,word.substr(1),count);
+    }
+};
 class Solution {
 public:
+ TrieNode* root = new TrieNode('-');
     string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
-        string ans = strs[0];  // Initialize with the first string
-        
-        for (int i = 1; i < n; i++) {  // Start from the second string
-            int j = 0;
-            // Compare characters of ans and strs[i] while they match
-            while (j < ans.size() && j < strs[i].size() && ans[j] == strs[i][j]) {
-                j++;
-            }
-            ans = ans.substr(0, j);  // Update ans to the common prefix found
-            if (ans == "") return "";  // If no common prefix, return empty string
+        auto s = strs[0];
+      root->insertWord(root,s);
+       
+        int ans = INT_MAX;
+        for(int i=1;i<strs.size();i++){
+            auto word = strs[i];
+             int count = 0;
+          root->search(root,word,count);
+            ans = min(ans,count);
         }
+        return strs[0].substr(0,ans);
         
-        return ans;
     }
 };
