3ddf1222fbaa09ccd9e16282465a814fb4d955f1
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-03-23 17:49:26 +0530

    Time: 4 ms (55.43%), Space: 8.4 MB (10.14%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//method-1 recursion
 //solved using recursion
   int solvebyRecursion(int n){
 //base case
@@ -11,6 +12,7 @@ public:
  }
 
  //solved using DP
+ //method-2 using top-down approach
  int solveMemo(int n,vector<int>&dp){
     if(n==0 || n==1) return n;
 // step-3 - if ans exist no need to solve again just return the ans
@@ -21,10 +23,30 @@ if(dp[n]!=-1){
     dp[n] =solveMemo(n-1,dp) + solveMemo(n-2,dp);
     return dp[n];
  }
+
+ // method-3 using bottom -up approach
+ int solveTabulation(int n){
+    //step-1 create a dp array
+    vector<int>dp(n+1,-1);
+    //step-2  base case analyse,copy paste dp update
+    if(n==0 || n==1) return n;
+    dp[0] =0;
+    dp[1] =1;
+    //step-3 check parameter range ,reverse it , and run a loop over it
+  //  recursion->n->0;
+// reverse->0->n
+for(int i=2;i<=n ;i++){
+    dp[i] = dp[i-1] + dp[i-2];
+}
+return dp[n];
+ }
     int fib(int n) {
  //top-down approach
 //step-1 create a DP array here 1d dp will used
-vector<int>dp(n+1,-1);
-return solveMemo(n,dp);
+// vector<int>dp(n+1,-1);
+// return solveMemo(n,dp);
+
+//bottom -up approach
+ return solveTabulation(n);
     }
 };
