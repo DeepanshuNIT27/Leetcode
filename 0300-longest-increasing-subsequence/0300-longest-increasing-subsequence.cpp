afbad0492833b5faad4d3a4f1379d2a944a3dcdb
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-03-24 10:48:31 +0530

    Time: 655 ms (5.07%), Space: 296.5 MB (11.35%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,46 +1,70 @@
 class Solution {
 public:
-//REcursion ka code
-//  int solve(vector<int>&nums, int prevIndex , int i){
-//     //base case
-//     if(i>= nums.size()){
-//         return 0;
-//     }
-//     //include
-//     int incl = 0;
-//     if(prevIndex == -1 || nums[i]> nums[prevIndex]){
-//         incl = 1 + solve(nums,i,i+1);
-//     }
-//     //exclude
-//     int excl = 0 + solve(nums,prevIndex,i+1);
-//     int ans = max(incl,excl);
-//     return ans;
+    int solveUsingRec(vector<int>& arr, int curr, int prev){
+        //base case
+        if(curr >= arr.size()) {
+            return 0;
+        }
+        //prev-> index of element that was included in subsequence latest in the traversal
 
-//  }
-// 2d .dp
-int solveMemo(vector<int>&nums, int lastIndex , int i,vector<vector<int>>&dp){
-    //base case
-    if(i>= nums.size()){
-        return 0;
-    }
-    //include
-    if(dp[lastIndex+1][i] != -1) return dp[lastIndex+1][i];
-    int incl = 0;
-    if(lastIndex == -1 || nums[i]> nums[lastIndex]){
-        incl = 1 + solveMemo(nums,i,i+1,dp);
-    }
-    //exclude
-    int excl = 0 + solveMemo(nums,lastIndex,i+1,dp);
-    dp[lastIndex+1][i] = max(incl,excl);
-    return dp[lastIndex+1][i];
+        int inc = 0;
+        if(prev == -1 || arr[curr] > arr[prev] ){
+            inc = 1 + solveUsingRec(arr,curr+1, curr);
+        }
+        int exc = 0 + solveUsingRec(arr, curr+1, prev);
+        int finalAns = max(inc,exc);
+        return finalAns;
+    }//2d dp
 
- }
+    int solveUsingMem(vector<int>& arr, int curr, int prev, vector<vector<int> >& dp){
+        //base case
+        if(curr >= arr.size()) {
+            return 0;
+        }
+        if(dp[curr][prev+1] != -1) {
+            return dp[curr][prev+1];
+        }
+        //prev-> index of element that was included in subsequence latest in the traversal
+
+        int inc = 0;
+        if(prev == -1 || arr[curr] > arr[prev] ){
+            inc = 1 + solveUsingMem(arr,curr+1, curr, dp);
+        }
+        int exc = 0 + solveUsingMem(arr, curr+1, prev, dp);
+        dp[curr][prev+1] = max(inc,exc);
+        return dp[curr][prev+1];
+    }
+
+    int solveUsingTab(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int> > dp(n+1, vector<int>(n+1, 0));
+        
+        //Rec ranges
+        //curr -> 0 to n
+        //prev -> -1 to curr
+        //reverse it and apply loop 
+        for(int curr = n-1; curr>=0; curr--) {
+            for(int prev = curr-1; prev>=-1; prev--) {
+                int inc = 0;
+                if(prev == -1 || arr[curr] > arr[prev] ){
+                    inc = 1 + dp[curr+1][curr+1];
+                }
+                int exc = 0 + dp[curr+1][prev+1];
+                dp[curr][prev+1] = max(inc,exc);
+            } 
+        }
+
+        return dp[0][0];
+    }
+
     int lengthOfLIS(vector<int>& nums) {
-        int lastIndex = -1;
-    //   return   solve(nums,prevIndex,0);
-    //2d dp
-    int n = nums.size();
-vector<vector<int>>dp(n+2 ,vector<int>(n+1,-1));
- return    solveMemo(nums,lastIndex,0,dp);
+        int curr = 0;
+        //lastIndex
+        int prev = -1;
+        //return solveUsingRec(nums,curr,prev);
+        //OBSERVATION: prev hmesha curr se piche rehne wala h 
+        int n = nums.size();
+        vector<vector<int> > dp(n+1, vector<int>(n+1, -1));
+        return solveUsingMem(nums,curr,prev, dp);
     }
 };
