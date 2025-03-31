dbfa4ffea240330f82553275a19c96be416ee5d3
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-03-31 18:55:07 +0530

    Time: 50 ms (37.76%), Space: 29.4 MB (10.1%) - LeetHub

---
+++ 1367-maximum-height-by-stacking-cuboids/1367-maximum-height-by-stacking-cuboids.cpp
@@ -0,0 +1,77 @@
+class Solution {
+public:
+  bool isSafe(vector<int>prev ,vector<int>curr){
+  if(prev[0]<=curr[0] && prev[1]<=curr[1] && prev[2]<=curr[2]){
+    return true;
+  }
+  else return false;
+
+  return true;
+  }
+  //Using recursion
+  int solveUsingRec(vector<vector<int>>& cuboids, int curr , int prev){
+    //BASE CASE
+    if(curr >= cuboids.size()) return 0;
+     int inc = 0;
+     if(prev == -1  || isSafe(cuboids[prev], cuboids[curr])){
+        inc = cuboids[curr][2] + solveUsingRec(cuboids, curr+1 ,curr);
+     }
+      int exc = 0 + solveUsingRec(cuboids,curr+1,prev);
+
+      return max(inc,exc);
+  }
+
+  //memo
+  int solveUsingMemo(vector<vector<int>>& cuboids, int curr , int prev,vector<vector<int>>&dp){
+    //BASE CASE
+    if(curr >= cuboids.size()) return 0;
+    if(dp[curr][prev+1]!= -1) return dp[curr][prev];
+     int inc = 0;
+     if(prev == -1  || isSafe(cuboids[prev], cuboids[curr])){
+        inc = cuboids[curr][2] + solveUsingMemo(cuboids, curr+1 ,curr,dp);
+     }
+      int exc = 0 + solveUsingMemo(cuboids,curr+1,prev,dp);
+
+      dp[curr][prev+1] = max(inc,exc);
+      return dp[curr][prev+1];
+  }
+  // tabulation 
+
+  int solveUsingTabu(vector<vector<int>>& cuboids){
+     int n = cuboids.size();
+     vector<vector<int>>dp(n+1, vector<int>(n+1,0));
+    for(int curr = n-1;curr>=0 ;curr--){
+        for(int prev = curr-1; prev>=-1;prev--){
+         int inc = 0;
+     if(prev == -1  || isSafe(cuboids[prev], cuboids[curr])){
+        inc = cuboids[curr][2] + dp[curr+1][curr+1];
+     }
+      int exc = 0 +dp[curr+1][prev+1];
+
+      dp[curr][prev+1] = max(inc,exc);
+        }
+    }
+      return dp[0][0];
+  }
+    int maxHeight(vector<vector<int>>& cuboids) {
+       
+        for(auto & cuboid:cuboids){
+            sort(cuboid.begin(),cuboid.end());
+        }
+        sort(cuboids.begin(),cuboids.end());
+         int prev = -1;
+        int curr = 0;
+        //recursion
+        // int ans = solveUsingRec(cuboids,curr,prev); //recursive call
+        // return ans;
+
+        //Memo
+        int n = cuboids.size();
+        vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
+        // return solveUsingMemo(cuboids,curr,prev,dp);
+
+        //tabulation
+        return solveUsingTabu(cuboids);
+
+    }
+};
