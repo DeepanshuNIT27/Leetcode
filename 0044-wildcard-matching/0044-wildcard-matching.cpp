ee6d6733e42f1a3141c490e8f4e817d34365d338
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-04-05 20:15:36 +0530

    Time: 35 ms (44.67%), Space: 32.6 MB (5.04%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -62,16 +62,89 @@ public:
      dp[i][j] = ans;   
   return  dp[i][j];
   }
+  //tabulation
+  bool solveTab(string s, string p, int m,int n){
+    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+    dp[m][n] = true;
+   
+   for(int col =0 ;col<n ;col++){
+    bool flag = true;
+        for(int k=col ;k<p.length();k++){
+            if(p[k] !='*'){
+            flag = false;
+            break;
+            }
+        }
+         dp[m][col] = flag;
+    }
+
+
+   for(int i=m-1;i>=0;i--){
+    for(int j=n-1 ;j>=0 ;j--){
+     bool ans ;
+     if(p[j] =='?' || s[i]==p[j]){
+       ans =  dp[i+1][j+1];
+    }
+    else if( p[j]=='*'){
+     ans =  dp[i+1][j] || dp[i][j+1];
+    }
+    else{
+        ans = false;
+    }
+     dp[i][j] = ans;   
+    }
+   }
+  return  dp[0][0];
+  }
+//space opti
+bool solveTabSO(string s, string p, int m,int n){
+   vector<int>next(n+1,0);
+   vector<int>curr(n+1,0);
+    next[n] = true;
+   
+   for(int col =0 ;col<n ;col++){
+    bool flag = true;
+        for(int k=col ;k<p.length();k++){
+            if(p[k] !='*'){
+            flag = false;
+            break;
+            }
+        }
+         next[col] = flag;
+    }
+
+
+   for(int i=m-1;i>=0;i--){
+    for(int j=n-1 ;j>=0 ;j--){
+     bool ans ;
+     if(p[j] =='?' || s[i]==p[j]){
+       ans =  next[j+1];
+    }
+    else if( p[j]=='*'){
+     ans =  next[j] || curr[j+1];
+    }
+    else{
+        ans = false;
+    }
+     next[j] = ans;   
+    }
+    curr = next;
+   }
+  return  next[0];
+  }
 
     bool isMatch(string s, string p) {
-        int n = s.size();
-        int m = p.size();
+        int m = s.size();
+        int n = p.size();
         //recursive
        // return solveRec(s,p,0,0);
 
        //memorization
-       vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-       return solveMemo(s,p,0,0,dp);
+    //    vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+    //    return solveMemo(s,p,0,0,dp);
+
+       //tabulation 
+      return  solveTab(s,p,m,n);
         
     }
 };
