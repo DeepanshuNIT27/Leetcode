54c685ae258ec22435a98593b4d178df554414f4
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-07-05 09:01:57 +0530

    Time: 0 ms (100%), Space: 13.9 MB (37.49%) - LeetHub

---
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int,int>mp;
+        for(auto&it:arr) mp[it]++;
+        int ans = -1;
+        for(auto it=mp.begin();it!=mp.end();it++){
+           if(it->first==it->second) ans = max(ans,it->first);
+        }
+      return ans;  
+    }
+};
