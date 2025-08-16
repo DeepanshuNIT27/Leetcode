2c5e1ab54afb7c1a017d69396ebdc2f128545bdb
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-08-16 11:14:35 +0530

    Time: 5 ms (6.7%), Space: 12.8 MB (5.67%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
+ void removeocc(string &s , string &part){
+    if(s.size()==0 || s.find(part)==string::npos){
+        return ;
+    }
+    //1 case hm smbhalenge
+    int index = s.find(part);
+    if(index!=string::npos){
+        //string jodna better option rather than erase
+        string right = s.substr(0,index);
+        string left = s.substr(index+part.size(),s.size());
+        s= right+left;
+     
+    }
+    removeocc(s,part);
+     
+ }
     string removeOccurrences(string s, string part) {
-        while(s.find(part)!=string::npos){
-            s.erase(s.find(part),part.length());
-        }
-        return s;
-        
+        removeocc(s,part);
+return s;
     }
 };
