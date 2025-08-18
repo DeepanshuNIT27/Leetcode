66225f31173a8e8ed8832045fa450a5f666c88a8
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-08-18 19:24:18 +0530

    Time: 4 ms (18%), Space: 97.2 MB (86.9%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,13 +1,21 @@
 class Solution {
-public:
+public: 
+ void solve(vector<int>& prices,int &mini , int & ans , int index){
+      
+      //base case 
+      if(index>=prices.size()){
+        return;
+      }
+        int curr = prices[index];
+        mini = min(curr,mini);
+        ans = max((curr-mini),ans);
+        solve(prices,mini,ans,index+1);
+ }
     int maxProfit(vector<int>& prices) {
         int mini = INT_MAX;
         int ans = INT_MIN;
-        for(int i=0;i<prices.size();i++){
-            int curr = prices[i];
-            mini = min(curr,mini);
-            ans = max((curr-mini),ans);
-        }
-        return ans;
+        solve(prices,mini,ans,0);
+return ans;
     }
 };
+ 
