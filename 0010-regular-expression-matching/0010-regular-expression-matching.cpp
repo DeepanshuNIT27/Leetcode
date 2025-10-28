c645379e1a410bb9ef39e66ab79c5dbd9dd8456b
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-10-28 13:23:17 +0530

    Time: 3 ms (50.08%), Space: 9.6 MB (26.49%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//RECURSION
 bool solveRec(string &s , string &p , int i , int j){
     //BASE CASE
     if(i == s.size() && j == p.size()) {
@@ -8,6 +9,7 @@ bool solveRec(string &s , string &p , int i , int j){
     if(j>=p.size()){
         return false;
     }
+    //3rd case lene ki jarurat ni hai multiple * wala vo case bnega hi ni 
 
     bool currmatch = ( i<s.size() && ((s[i]==p[j]) || (p[j]=='.')));
     if(j+1<p.size() && p[j+1]=='*'){
@@ -23,7 +25,43 @@ bool solveRec(string &s , string &p , int i , int j){
     }
    
 }
+//MEMOIZATION
+bool solveMemo(string &s , string &p , int i , int j, vector<vector<int>>&dp){
+    //BASE CASE
+    if(i == s.size() && j == p.size()) {
+         return true;
+    }
+    if(j>=p.size()){
+        return false;
+    }
+
+    //3rd case lene ki jarurat ni hai multiple * wala vo case bnega hi ni 
+    if(dp[i][j]!=-1) return dp[i][j];
+    bool ans ;
+    bool currmatch = ( i<s.size() && ((s[i]==p[j]) || (p[j]=='.')));
+    if(j+1<p.size() && p[j+1]=='*'){
+         bool EmptyWala = solveMemo(s,p,i,j+2,dp);
+         bool PreElementWala = currmatch &&  solveMemo(s,p,i+1,j,dp);
+         ans =  EmptyWala || PreElementWala;
+    }
+    else if(currmatch){
+         ans = solveMemo(s,p,i+1,j+1,dp);
+    }
+    else {
+        ans =  false;
+    }
+ dp[i][j] = ans;
+   return dp[i][j];
+}
+
     bool isMatch(string s, string p) {
-        return solveRec(s,p,0,0);
+        //RECURSION
+        //return solveRec(s,p,0,0);
+
+        //MEMOIZATION
+        int n = s.size();
+        int m = p.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solveMemo(s,p,0,0,dp);
     }
 };
