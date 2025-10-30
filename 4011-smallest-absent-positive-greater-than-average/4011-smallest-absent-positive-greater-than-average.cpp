069390c3b4559de72c89c276a2e5ce0806150d22
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-10-30 07:43:18 +0530

    Time: 3 ms (85.84%), Space: 85.5 MB (90.67%) - LeetHub

---
+++ 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int smallestAbsent(vector<int>& nums) {
+        float sum = accumulate(nums.begin(),nums.end(),0LL);
+         float avg =  sum / nums.size();
+         sort(nums.begin(),nums.end());
+         auto it = upper_bound(nums.begin(),nums.end(),0);
+         if(it==nums.end()) return 1;
+         int index =  it - nums.begin();
+         int first = nums[index];
+         
+         for(int i = 1;i<=nums.back();i++ ){
+            if(count(nums.begin(),nums.end(),i)>0) continue;
+            else {
+                if(i>avg) return i;
+            }
+         }
+        return nums.back() + 1;
+    }
+};
