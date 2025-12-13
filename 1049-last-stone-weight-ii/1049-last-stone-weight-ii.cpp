8b5effb0d48d4b1bf3ee8bd93e9557efbaa74519
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-12-13 14:39:53 +0530

    Time: 7 ms (12.17%), Space: 13.7 MB (15.08%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -25,14 +25,35 @@ int exc = solveMemo(stones,i+1,sum,totalsum,dp);
  dp[i][sum] = min(inc,exc);
  return dp[i][sum];
 }
+//Tabulation 
+int solveTab(vector<int>& stones){
+     int totalsum = accumulate(stones.begin(),stones.end(),0);
+     vector<vector<int>>dp(stones.size()+1,vector<int>(totalsum+1,-1));
+     for(int sum=0;sum<=totalsum ;sum++){
+        dp[stones.size()][sum] = abs(sum - totalsum + sum);
+     }
+     for(int i=stones.size()-1;i>=0;i--){
+        for(int j=totalsum ;j>=0;j--){
+            int inc = INT_MAX ;
+    if(j+stones[i]<=totalsum)   
+      inc = dp[i+1][j+stones[i]];
+      int exc = dp[i+1][j];
+      dp[i][j] = min(inc,exc);
+        }
+     }
+     return dp[0][0];
+}
 
     int lastStoneWeightII(vector<int>& stones) {
       int totalsum = accumulate(stones.begin(),stones.end(),0);
       //Recursion
     //   return    solveRec(stones,0,0,totalsum);
     //Memoization
-     vector<vector<int>>dp(stones.size()+1,vector<int>(totalsum+1,-1));
-     return solveMemo(stones,0,0,totalsum,dp);
+    //  vector<vector<int>>dp(stones.size()+1,vector<int>(totalsum+1,-1));
+    //  return solveMemo(stones,0,0,totalsum,dp);
+
+    //Tabulation
+    return solveTab(stones);
 
     }
 };
