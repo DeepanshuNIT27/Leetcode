05f5c4caad8610cdee239e529c4a594741d999fd
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-12-13 14:28:35 +0530

    Time: 0 ms (100%), Space: 13.8 MB (15.08%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.cpp
@@ -1,85 +1,38 @@
 class Solution {
 public:
-//Method 1
-//REcursive soln 
- int solve(vector<int>& stones, int index , int sum){
-    if(index == stones.size()){
-        if(sum<0) return INT_MAX;
-        return sum;
-    }
-    int pos = solve(stones,index+1, sum+stones[index]);
-    int neg = solve(stones,index+1,sum-stones[index]);
+//Method -2 grouping method
+//RECURSION
+int solveRec(vector<int>& stones , int i , int sum,int totalsum){
+ if(i==stones.size()){
+ int g1sum = sum;
+ int g2sum = totalsum-sum;
+ return abs(g1sum - g2sum);
+}
+int inc = solveRec(stones,i+1,sum+stones[i],totalsum);
+int exc = solveRec(stones,i+1,sum,totalsum);
+return min(inc,exc);
+}
+//MEMOIZATION
+int solveMemo(vector<int>& stones , int i , int sum,int totalsum, vector<vector<int>>&dp){
+ if(i==stones.size()){
+ int g1sum = sum;
+ int g2sum = totalsum-sum;
+ return abs(g1sum - g2sum);
+}
+if(dp[i][sum]!=-1) return dp[i][sum];
+int inc = solveMemo(stones,i+1,sum+stones[i],totalsum,dp);
+int exc = solveMemo(stones,i+1,sum,totalsum,dp);
+ dp[i][sum] = min(inc,exc);
+ return dp[i][sum];
+}
 
-    return min(pos,neg);
- }
-
- //Memoization
-   int solveMemo(vector<int>& stones, int index , int sum,int totalsum,  vector<vector<int>>&dp){
-    if(index == stones.size()){
-        if(sum<0) return INT_MAX;
-        return sum;
-    }
-    if(dp[index][sum+totalsum]!=-1) return dp[index][sum+totalsum];
-    int pos = solveMemo(stones,index+1, sum+stones[index],totalsum,dp);
-    int neg = solveMemo(stones,index+1,sum-stones[index],totalsum,dp);
-
-    dp[index][sum+totalsum] = min(pos,neg);
-    return dp[index][sum+totalsum];
- }
-//Tabulation
-  int solveTab(vector<int>& stones){
-    int totalsum = accumulate(stones.begin(),stones.end(),0);
-        vector<vector<int>>dp(stones.size()+1,vector<int>(2*totalsum+1,0));
-    for(int sum =-totalsum ; sum<=totalsum;sum++){
-        dp[stones.size()][sum+totalsum] = sum<0 ? INT_MAX : sum;
-    }
-    for(int i=stones.size()-1;i>=0;i--){
-        for(int j=totalsum ;j>= -totalsum ;j--){
-             int pos = INT_MAX ; int neg = INT_MAX;
-            if(j+stones[i]<=totalsum)
-              pos = dp[i+1][totalsum +j+stones[i]];
-            if(j-stones[i]>=-totalsum)
-              neg =  dp[i+1][totalsum +j-stones[i]];
-             dp[i][j+totalsum] =  min(pos,neg);
-        }
-
-    }
-    return dp[0][totalsum];
- }
-
- //Space optimization
-
- int solveSO(vector<int>& stones){
-    int totalsum = accumulate(stones.begin(),stones.end(),0);
-    vector<int>curr(2*totalsum+1,0);
-     vector<int>next(2*totalsum+1,0);
-
-    for(int sum =-totalsum ; sum<=totalsum;sum++){
-        next[sum+totalsum] = sum<0 ? INT_MAX : sum;
-    }
-    for(int i=stones.size()-1;i>=0;i--){
-        for(int j=totalsum ;j>= -totalsum ;j--){
-             int pos = INT_MAX ; int neg = INT_MAX;
-            if(j+stones[i]<=totalsum)
-              pos = next[totalsum +j+stones[i]];
-            if(j-stones[i]>=-totalsum)
-              neg =  next[totalsum +j-stones[i]];
-             curr[j+totalsum] =  min(pos,neg);
-        }
-      next = curr;
-    }
-    return curr[totalsum];
- }
     int lastStoneWeightII(vector<int>& stones) {
-        //recursion
-       //  return solve(stones,0,0);
-
-        //Memoization
-    //    int totalsum = accumulate(stones.begin(),stones.end(),0);
-    //     vector<vector<int>>dp(stones.size()+1,vector<int>(2*totalsum+1,-1));
-        // return solveMemo(stones,0,0,totalsum,dp);
-      //  return solveTab(stones);
-      return solveSO(stones);
+      int totalsum = accumulate(stones.begin(),stones.end(),0);
+      //Recursion
+    //   return    solveRec(stones,0,0,totalsum);
+    //Memoization
+     vector<vector<int>>dp(stones.size()+1,vector<int>(totalsum+1,-1));
+     return solveMemo(stones,0,0,totalsum,dp);
 
     }
 };
