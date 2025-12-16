d550c0ee4bcdf11fe8cb3ad6a336ab6d118b759d
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-12-16 12:11:16 +0530

    Time: 52 ms (52.44%), Space: 13.3 MB (43.49%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -27,6 +27,21 @@ int solveMemo(int n, vector<int>&perfect,vector<int>&dp ){
      dp[n] = ans;
     return dp[n];
 }
+// Tabulation Method
+int solveTab(int n , vector<int>&perfect){
+    vector<int>dp(n+1,0);
+    dp[0] = 0;
+    for(int i=1;i<=n;i++){
+    int ans= 1e9;
+    for(int j=0;j<perfect.size();j++){
+       if(perfect[j]>i) break;
+      ans = min(ans , 1+ dp[i-perfect[j]]);
+     
+    }
+     dp[i] = ans;  
+}
+     return dp[n];
+}
 
 
     int numSquares(int n) {
@@ -37,8 +52,11 @@ int solveMemo(int n, vector<int>&perfect,vector<int>&dp ){
         //RECURSION
   //return  solveRec(n,perfect);
      //   Memoization
-        vector<int>dp(n+1,-1);
-    return solveMemo(n,perfect,dp);
+   //     vector<int>dp(n+1,-1);
+   // return solveMemo(n,perfect,dp);
+
+    //Tabulation
+    return solveTab(n,perfect);
 
     }
 };
