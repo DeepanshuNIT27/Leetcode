d3750edbe480d2c4092533e821985a1543daa01e
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2025-12-23 14:30:26 +0530

    Time: 35 ms (28.69%), Space: 44.6 MB (9.84%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -31,6 +31,24 @@ int solveMemo(string&s, string&t, int i , int j,vector<vector<int>>&dp){
     dp[i][j] = ans;
     return dp[i][j];
 }
+//Tabulation
+int solveTab(string&s , string&t){
+    vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,0));
+    for(int i=0;i<=s.size();i++) dp[i][t.size()] = 1;
+    for(int i=s.size()-1;i>=0;i--){
+        for(int j=t.size()-1;j>=0;j--){
+            long long ans = 0LL;
+           if(s[i]==t[j]){
+            //dono bdhao
+          ans += dp[i+1][j+1];
+    }
+           //i ko bs bdao
+         ans += dp[i+1][j];
+         dp[i][j] = ans;
+        }
+    }
+ return dp[0][0];
+}
 
     int numDistinct(string s, string t) {
         //recursion
@@ -38,7 +56,10 @@ int solveMemo(string&s, string&t, int i , int j,vector<vector<int>>&dp){
 
         //Memoization
         vector<vector<int>>dp(s.size()+1,vector<int>(t.size()+1,-1));
-        return solveMemo(s,t,0,0,dp);
+      //  return solveMemo(s,t,0,0,dp);
+
+        //Tabulation
+        return solveTab(s,t);
 
     }
 };
