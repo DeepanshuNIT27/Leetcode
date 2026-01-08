e8a715bbf1e92cf0169e6882d6795f11edb0030a
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-01-08 12:56:16 +0530

    Time: 0 ms (100%), Space: 77.1 MB (5.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,30 +1,19 @@
 class Solution {
 public:
- int maxSumHelper(vector<int>&nums , int start , int end){
-    if(start == end) return nums[start];
-    int MaxLeftBoarderSum = INT_MIN;
-    int MAXRightBoarderSum = INT_MIN;
-     int mid  = start +(end-start)/2;
-    int maxLeftSum = maxSumHelper(nums,start,mid);
-    int maxRightSum = maxSumHelper(nums,mid+1,end);
-
-    //cross sum
-    int leftBoarderSum = 0 , rightBoarderSum = 0;
-    for(int i=mid;i>=start;i--){
-        leftBoarderSum += nums[i];
-        if(leftBoarderSum>MaxLeftBoarderSum) MaxLeftBoarderSum = leftBoarderSum;
-    }
-    for(int i=mid+1;i<=end;i++){
-       rightBoarderSum += nums[i];
-        if(rightBoarderSum>MAXRightBoarderSum) MAXRightBoarderSum = rightBoarderSum;
-    }
-    int crossBoarderSum =MaxLeftBoarderSum  + MAXRightBoarderSum;
-    return max(crossBoarderSum,max( maxLeftSum,maxRightSum));
- }
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int start = 0;
-        int end = n-1;
-        return maxSumHelper(nums,start,end);
+        vector<long long >pre(n);
+        pre[0] = nums[0];
+        for(int i=1;i<n;i++) {
+            pre[i] = pre[i-1] +nums[i];
+        }
+        long long bestOfMin = 0LL;
+        long long ans = -1e18;
+        for(int i=0;i<n;i++){
+            long long best = pre[i] - bestOfMin;
+            bestOfMin = min(bestOfMin, pre[i]);
+            ans = max(ans,best);
+        }
+        return ans;
     }
 };
