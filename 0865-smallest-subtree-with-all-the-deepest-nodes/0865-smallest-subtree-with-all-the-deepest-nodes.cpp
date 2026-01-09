e09b119948774459cd391e933d430d185f99b6c0
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-01-09 10:30:52 +0530

    Time: 0 ms (100%), Space: 16.2 MB (52.91%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ int heightdfs(TreeNode* root){
+    if(root==nullptr) return 0; 
+    int leftheight = heightdfs(root->left);
+    int rightheight = heightdfs(root->right);
+     return max(leftheight,rightheight) + 1;
+ }
+ bool solvedfs(TreeNode* &root, TreeNode* &temp,int &height , int x){
+    if(root == nullptr) return false;
+    if( root->left == nullptr && root->right ==nullptr && x == height){
+        temp = root;
+        return true;
+    }
+   bool ans1 =  solvedfs(root->left,temp,height,x+1);
+   bool ans2 =  solvedfs(root->right,temp,height,x+1);
+   if(ans1 && ans2) temp = root;
+   return ans1 || ans2 ;
+ }
+class Solution {
+public:
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int height = heightdfs(root);
+       TreeNode* temp = root;
+       solvedfs(root,temp,height,1);
+       return temp;
+    }
+};
