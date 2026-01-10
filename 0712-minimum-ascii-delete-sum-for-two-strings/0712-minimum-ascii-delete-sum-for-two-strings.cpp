0c7120fb72fa9bd64e717374055a2c949ac06396
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-01-10 08:32:52 +0530

    Time: 39 ms (20.02%), Space: 18.5 MB (9.87%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,87 +1,58 @@
 class Solution {
 public:
 //Recursion
-int solveRec(string &s1,string &s2 , int i , int j){
-    int cost = 0;
-    //dono me se koi bhi khtm ho jaye to
-    if(i==s1.size() || j==s2.size()){
-        for(int x=i;x<s1.size();x++) cost += s1[x];
-        for(int x=j;x<s2.size();x++) cost += s2[x];
-    }
-    // char match kr jaye
-    else if (s1[i]==s2[j]) cost  = solveRec(s1,s2,i+1,j+1);
-
-    // no match
+int solveRec(string &s1, string &s2 , int i , int j){
+    //BASE CASE
+    int ans = 0;
+    if(i==s1.size() || j==s2.size()) {
+        for(int k=i;k<s1.size();k++)  ans += s1[k];
+        for(int k=j;k<s2.size();k++) ans += s2[k];
+     }
+     //dono same hai 
+   else if(s1[i]==s2[j])  ans  = solveRec(s1,s2,i+1,j+1);
     else {
-        int cost1 = s1[i] + solveRec(s1,s2,i+1,j);
-        int cost2 = s2[j] + solveRec(s1,s2,i,j+1);
-        cost = min(cost1,cost2);
+    // s1 delte hoga 
+      int  ans1 = s1[i] + solveRec(s1,s2,i+1,j);
+    //s2 delete hoga
+      int  ans2 = s2[j] + solveRec(s1,s2,i,j+1);
+
+      ans = min(ans1,ans2);
     }
-    return cost;
+    return ans;
 }
 //Memoization
-int solveMemo(string &s1,string &s2 , int i , int j,vector<vector<int>>&dp){
-    int cost = 0;
-    //dono me se koi bhi khtm ho jaye to
+int solveMemo(string &s1, string &s2 , int i , int j,vector<vector<int>>&dp){
+    //BASE CASE
+    int ans = 0;
     if(dp[i][j]!=-1) return dp[i][j];
-    if(i==s1.size() || j==s2.size()){
-        for(int x=i;x<s1.size();x++) cost += s1[x];
-        for(int x=j;x<s2.size();x++) cost += s2[x];
-    }
-    // char match kr jaye
-    else if (s1[i]==s2[j]) cost  = solveMemo(s1,s2,i+1,j+1,dp);
-
-    // no match
+    if(i==s1.size() || j==s2.size()) {
+        for(int k=i;k<s1.size();k++)  ans += s1[k];
+        for(int k=j;k<s2.size();k++) ans += s2[k];
+     }
+     //dono same hai 
+   else if(s1[i]==s2[j])  ans  =solveMemo(s1,s2,i+1,j+1,dp);
     else {
-        int cost1 = s1[i] + solveMemo(s1,s2,i+1,j,dp);
-        int cost2 = s2[j] + solveMemo(s1,s2,i,j+1,dp);
-        cost = min(cost1,cost2);
+    // s1 delte hoga 
+      int  ans1 = s1[i] + solveMemo(s1,s2,i+1,j,dp);
+    //s2 delete hoga
+      int  ans2 = s2[j] + solveMemo(s1,s2,i,j+1,dp);
+
+      ans = min(ans1,ans2);
     }
-    dp[i][j] = cost;
+    dp[i][j] = ans;
     return dp[i][j];
 }
-//Tabulation
-int solveTab(string &s1, string &s2){
-    vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,0));
-    int temp = 0;
-    for(int i=s2.size()-1;i>=0;i--){
-        temp += s2[i];
-       dp[s1.size()][i] = temp;
-    }
-    temp = 0;
-     for(int i=s1.size()-1;i>=0;i--){
-        temp += s1[i];
-       dp[i][s2.size()] = temp;
-    }
-    
-    for(int i=s1.size()-1;i>=0;i--){
-        for(int j=s2.size()-1;j>=0;j--){
-          int cost = 0;
-        if (s1[i]==s2[j]) cost  = dp[i+1][j+1];
-        else {
-        int cost1 = s1[i] + dp[i+1][j];
-        int cost2 = s2[j] +dp[i][j+1];
-        cost = min(cost1,cost2);
-        }
-         dp[i][j] = cost;
-        }
-    }
-return dp[0][0];
-}
-
 
     int minimumDeleteSum(string s1, string s2) {
-        //Recursion 
-     // return   solveRec(s1,s2,0,0);
+        int n = s1.size();
+        int m = s2.size();
+        //Recursion
+     // return  solveRec(s1,s2,0,0);
 
-      //memoization
-      vector<vector<int>>dp(s1.size()+1,vector<int>(s2.size()+1,-1));
-    //  return solveMemo(s1,s2,0,0,dp);
+     //Memoization
+     vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+     return solveMemo(s1,s2,0,0,dp);
 
-      // Tabulation
-    return solveTab(s1,s2);
-
-     
 
     }
 };
