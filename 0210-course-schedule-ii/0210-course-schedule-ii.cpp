9b02eb6ee620305b21af61d2022169002c9fd450
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-09 17:57:22 +0530

    Time: 9 ms (17.86%), Space: 19.8 MB (6.29%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-void topSort( unordered_map<int,list<int>>&adj, int n,vector<int>&ans){
-    //creating indegree
-    unordered_map<int,int>indegree;
-    for(auto a: adj){
-        for(auto b : a.second){
-            indegree[b]++;
-        }
-    }
-    //maintaining iniatal state
-    queue<int>q;
-    stack<int>st;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
-            q.push(i);
-        }
-    }
-    //main logic
-    while(!q.empty()){
-        auto frontNode = q.front();
-        q.pop();
-       ans.push_back(frontNode);
-        for(auto nbr:adj[frontNode]){
-            indegree[nbr]--;
-            if(indegree[nbr]==0){
-                q.push(nbr);
-            }
-        }
-    }
-    //cycle check
-    if(ans.size()!=n){
-        ans.clear();
-    }
-    
-
-}
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-         unordered_map<int,list<int>>adj;
-        for(int i=0;i<prerequisites.size();i++){
-            auto it = prerequisites[i];
-            int u = it[0];
-            int v = it[1];
-            adj[v].push_back(u);
+        queue<int>q;
+        vector<int>indegree(numCourses,0);
+        unordered_map<int,list<int>>Adj;
+        for(auto temp:prerequisites){
+            Adj[temp[1]].push_back(temp[0]);
+            indegree[temp[0]]++;
+        }
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
         }
         vector<int>ans;
-         topSort(adj, numCourses,ans);
-         return ans;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            ans.push_back(front);
+            for(auto nbr : Adj[front]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.push(nbr);
+            }
+        }
+        if(ans.size()==numCourses) return ans;
+        ans.clear();
+        return ans;
     }
-    
 };
