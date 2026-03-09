1695b23e00a5ce3463c9107163531ce0ba3a96c8
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-09 17:55:14 +0530

    Time: 8 ms (36.64%), Space: 21.5 MB (7.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,46 +1,27 @@
 class Solution {
 public:
-bool topSort( unordered_map<int,list<int>>&adj, int n){
-    //creating indegree
-    unordered_map<int,int>indegree;
-    for(auto a: adj){
-        for(auto b : a.second){
-            indegree[b]++;
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        queue<int>q;
+        vector<int>indegree(numCourses,0);
+        unordered_map<int,list<int>>Adj;
+        for(auto temp:prerequisites){
+            Adj[temp[1]].push_back(temp[0]);
+            indegree[temp[0]]++;
         }
-    }
-    //maintaining iniatal state
-    queue<int>q;
-    stack<int>st;
-    for(int i=0;i<n;i++){
-        if(indegree[i]==0){
-            q.push(i);
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0) q.push(i);
         }
-    }
-    //main logic
-    while(!q.empty()){
-        auto frontNode = q.front();
-        q.pop();
-        st.push(frontNode);
-        for(auto nbr:adj[frontNode]){
-            indegree[nbr]--;
-            if(indegree[nbr]==0){
-                q.push(nbr);
+        vector<int>ans;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            ans.push_back(front);
+            for(auto nbr : Adj[front]){
+                indegree[nbr]--;
+                if(indegree[nbr]==0) q.push(nbr);
             }
         }
-    }
-    if(st.size()==n) return true;
-    return false;
-
-}
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int,list<int>>adj;
-        for(int i=0;i<prerequisites.size();i++){
-            auto it = prerequisites[i];
-            int u = it[0];
-            int v = it[1];
-            adj[v].push_back(u);
-        }
-        bool ans = topSort(adj, numCourses);
-   return ans;
+        if(ans.size()==numCourses) return true;
+        return false;
     }
 };
