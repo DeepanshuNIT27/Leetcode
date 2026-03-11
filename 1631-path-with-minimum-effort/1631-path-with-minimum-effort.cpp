389c0f399b9be148b34bd91906df418cb636b503
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-11 17:55:43 +0530

    Time: 44 ms (82.45%), Space: 24.6 MB (56.43%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        
+         int n = heights.size();
+        int m = heights[0].size();
+
+        priority_queue<pair<int,pair<int,int>> , vector<pair<int,pair<int,int>>> , greater<pair<int,pair<int,int>>>>pq;
+   
+        pq.push({0,{0,0}});
+        vector<vector<int>>distance(n,vector<int>(m,INT_MAX));
+       distance[0][0] = 0;
+      
+        while(!pq.empty()){
+            auto front = pq.top();
+             int cost = front.first;
+             int x = front.second.first;
+             int y = front.second.second;
+
+             pq.pop();
+             if(x==n-1 && y==m-1) return cost;
+
+             int dx[] = {0,0,-1,1};
+             int dy[] = {1,-1,0,0};
+
+             for(int i=0;i<4;i++){
+                int newx = x + dx[i];
+                int newy = y + dy[i];
+
+             if(newx>=0 && newx<n && newy>=0 && newy<m){     
+               int d = max(cost,abs(heights[x][y] - heights[newx][newy]));
+               if(d<distance[newx][newy]){
+                distance[newx][newy] = d;
+                 pq.push({d,{newx,newy}});
+               }
+              
+
+                }
+             }    
+        }
+        return 0;
+    }
+};
