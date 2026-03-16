4d12ad35c2ca8c194a0aeff101dc57995f4e3298
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-16 10:26:30 +0530

    Time: 3 ms (13.26%), Space: 10.7 MB (44.58%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -22,10 +22,34 @@ vector<string>solveRec(string&s,  unordered_map<string,bool>&dict ,int i){
     return ans;
 }
 
+// Memoization
+    unordered_map<int,vector<string>>dp;
+    vector<string>solveMemo(string&s,  unordered_map<string,bool>&dict ,int i){
+    if(i== s.size()) return {""};
+   if(dp.find(i)!=dp.end()) return dp[i];
+    vector<string>ans;
+    string word ;
+    for(int j =i;j<s.size();j++){
+        word.push_back(s[j]);
+        if(dict.find(word)==dict.end()) continue;
+
+        //found a valid word;
+      auto right = solveRec(s,dict,j+1);
+        for(auto eachRightPart : right){
+            string endPart;
+            if(eachRightPart.size()>0) endPart = " " + eachRightPart;
+            ans.push_back(word + endPart);
+        }
+    }
+    dp[i] = ans;
+    return dp[i];
+}
+
     vector<string> wordBreak(string s, vector<string>& wordDict) {
         unordered_map<string,bool>dict;
         for(auto word : wordDict) dict[word] = 1;
+      //  return solveRec(s,dict,0);
 
-        return solveRec(s,dict,0);
+      return solveMemo(s,dict,0);
     }
 };
