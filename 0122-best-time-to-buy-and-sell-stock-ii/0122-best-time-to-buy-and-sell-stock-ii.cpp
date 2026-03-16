e0bdd5a6c7cf77d2dbe403ee843437277352f5eb
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-16 16:34:45 +0530

    Time: 20 ms (5.6%), Space: 28.9 MB (5.14%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -37,6 +37,27 @@ int  solveMemo(vector<int>& prices, int i ,int flag , vector<vector<int>>&dp){
     dp[i][flag] = max(max(ans1,ans2),ans3);
     return dp[i][flag];
 }
+int solveTab(vector<int>& prices){
+    int n = prices.size();
+    vector<vector<int>>dp(n+1,vector<int>(2,0));
+    
+    for(int i=n-1;i>=0;i--){
+        for(int flag = 0;flag<=1;flag++){
+     int   ans1 = 0;
+     int  ans2 = 0;
+     int ans3 = 0;
+   if(flag)  ans1 =  prices[i] + dp[i+1][0];
+    // buy 
+   if(flag == false) ans2 = - prices[i] + dp[i+1][1];
+    // nothing to do 
+     ans3 =  0 + dp[i+1][flag];
+
+    dp[i][flag] = max(max(ans1,ans2),ans3);
+    }
+    }
+    return dp[0][0];
+
+}
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
@@ -45,7 +66,10 @@ int  solveMemo(vector<int>& prices, int i ,int flag , vector<vector<int>>&dp){
 
      //memoization
      vector<vector<int>>dp(n+1,vector<int>(2,-1));
-     return solveMemo(prices,0,false,dp);
+     //return solveMemo(prices,0,false,dp);
+
+     //tabulation
+     return solveTab(prices);
 
 
     }
