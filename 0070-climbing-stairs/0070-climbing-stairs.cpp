426d6116e7320c6aa2b1239d1cca5f2d6c16b2f4
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-18 19:12:17 +0530

    Time: 0 ms (100%), Space: 8.4 MB (46.84%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+//recursion
+int  solveRec(int n){
+    if(n==0) return 1;
+    if(n<0) return 0;
+   int ans1 =  solveRec(n-1);
+   int ans2 =  solveRec(n-2);
+
+   return ans1+ans2;
+}
+
+//Memoization
+int solveMemo(int n , vector<int>&dp ){
+
+     if(n==0) return 1;
+    if(n<0) return 0;
+  if(dp[n]!=-1) return dp[n];
+   int ans1 =  solveMemo(n-1,dp);
+   int ans2 =  solveMemo(n-2,dp);
+
+   dp[n] = ans1 + ans2 ;
+   return dp[n];
+}
+    int climbStairs(int n) {
+      
+       // return solveRec(n);
+
+       vector<int>dp(n+1,-1);
+       return solveMemo(n,dp);
+    }
+};
