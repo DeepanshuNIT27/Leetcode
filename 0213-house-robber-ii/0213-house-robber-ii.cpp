8d5c3c74587a90378decdc4c0ce3cc0dedfc9747
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-19 19:47:45 +0530

    Time: 0 ms (100%), Space: 10.3 MB (63.23%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -20,6 +20,34 @@ int solveMemo(vector<int>& nums,int i , int n, vector<int>&dp){
     dp[i] =  max(inc,exc);
     return dp[i];
 }
+
+//tabulation
+int solveTab(vector<int>& nums,vector<int>&dp, int start , int end){
+
+    for(int i=end ; i>= start ;i--){
+         int inc = nums[i] + dp[i+2];
+         int exc = 0 + dp[i+1];
+
+         dp[i] = max(inc,exc);
+    }
+    return dp[start];
+}
+
+// space optimization 
+int solveSO(vector<int>& nums, int start , int end){
+    int prev1 = 0;
+    int prev2 = 0;
+    int curr ;
+    for(int i=end ; i>= start ;i--){
+         int inc = nums[i] + prev1;
+         int exc = 0 + prev2;
+
+         curr  = max(inc,exc);
+         prev1 = prev2;
+         prev2 = curr;
+    }
+    return prev2;
+}
     int rob(vector<int>& nums) {
         //Recursion 
         int n = nums.size();
@@ -29,13 +57,27 @@ int solveMemo(vector<int>& nums,int i , int n, vector<int>&dp){
    
        // return max(ans1,ans2);
 
-       //Memoization
-       vector<int>dp1(n+1,-1);
-       vector<int>dp2(n+1,-1);
-       int ans1 = solveMemo(nums,0,n-2,dp1);
-       int ans2 = solveMemo(nums,1,n-1,dp2);
+    //    //Memoization
+    //    vector<int>dp1(n+1,-1);
+    //    vector<int>dp2(n+1,-1);
+    //    int ans1 = solveMemo(nums,0,n-2,dp1);
+    //    int ans2 = solveMemo(nums,1,n-1,dp2);
+
+    //    return max(ans1,ans2);
+
+    // Tabulation
+    //     vector<int>dp1(n+2,0);
+    //    vector<int>dp2(n+2,0);
+    //    int ans1 = solveTab(nums,dp1,0,n-2);
+    //    int ans2 = solveTab(nums,dp2,1,n-1);
+
+    //    return max(ans1,ans2);
+
+    // space optimization ;
+
+     int ans1 = solveSO(nums,0,n-2);
+     int ans2 = solveSO(nums,1,n-1);
 
        return max(ans1,ans2);
-       
     }
 };
