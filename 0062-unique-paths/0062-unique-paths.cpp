b5e793b1b2a38633455f0ef3125841c93f4a9992
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-20 16:37:36 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.69%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -37,14 +37,41 @@ int solveMemo(int i, int j , int m , int n, vector<vector<int>>&dp){
      return dp[i][j];
 }
 
+// Tabulation 
+int solveTab(int m , int n ){
+vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+  dp[m-1][n-1] = 1;
+
+  for(int i=m-1;i>=0;i--){
+    for(int j=n-1;j>=0;j--){
+     if(i == m-1 && j==n-1) continue;
+     int ans = 0;
+     int dx[] = {0,1};
+     int dy[] = {1,0};
+     for(int k=0;k<2;k++){
+        if(isSafe(i+dx[k] , j+dy[k],m,n)){
+           ans += dp[i+dx[k]][j+dy[k]];
+        }
+     }
+      dp[i][j] = ans;
+    }
+   
+  }
+  return dp[0][0];
+}
+
+
 
     int uniquePaths(int m, int n) {
         //Recursion 
        // return solveRec(0,0,m,n);
 
        //Memoization
-       vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-       return solveMemo(0,0,m,n,dp);
+    //    vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+    //    return solveMemo(0,0,m,n,dp);
+
+       // Tabulation 
+       return solveTab(m,n);
 
     }
 };
