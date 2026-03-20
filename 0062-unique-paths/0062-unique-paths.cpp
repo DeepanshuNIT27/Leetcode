b547b0c21dfd82d4c0874cf6a10efccf740ec8cc
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-20 16:22:29 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.69%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+bool isSafe(int i , int j , int m , int n){
+    if(i>=0 && i<m && j>=0 && j<n) return true;
+    return false;
+}
+//Recursion soln 
+int solveRec(int i, int j , int m , int n){
+     if(i==m-1 && j==n-1) return 1;
+     if(i>=m || j>=n) return 0;
+     int ans = 0;
+     int dx[] = {0,1};
+     int dy[] = {1,0};
+     for(int k=0;k<2;k++){
+        if(isSafe(i+dx[k] , j+dy[k],m,n)){
+           ans += solveRec(i+dx[k] , j+dy[k] , m , n);
+
+        }
+     }
+     return ans;
+}
+//Memoization soln 
+int solveMemo(int i, int j , int m , int n, vector<vector<int>>&dp){
+     if(i==m-1 && j==n-1) return 1;
+     if(i>=m || j>=n) return 0;
+     if(dp[i][j]!=-1) return dp[i][j];
+     int ans = 0;
+     int dx[] = {0,1};
+     int dy[] = {1,0};
+     for(int k=0;k<2;k++){
+        if(isSafe(i+dx[k] , j+dy[k],m,n)){
+           ans += solveMemo(i+dx[k] , j+dy[k] , m , n,dp);
+
+        }
+     }
+     dp[i][j] =  ans;
+     return dp[i][j];
+}
+
+
+    int uniquePaths(int m, int n) {
+        //Recursion 
+       // return solveRec(0,0,m,n);
+
+       //Memoization
+       vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+       return solveMemo(0,0,m,n,dp);
+
+    }
+};
