029984f8efcde168fcea45cf1c5f917b68473732
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-20 17:02:02 +0530

    Time: 0 ms (100%), Space: 8.4 MB (74.43%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -60,6 +60,36 @@ vector<vector<int>>dp(m+1,vector<int>(n+1,0));
   return dp[0][0];
 }
 
+// space optimizatio
+int solveSO(int m , int n ){
+vector<int>next(n+1,0);
+vector<int>curr(n+1,0);
+  next[n-1] = 1;
+
+  for(int i=m-1;i>=0;i--){
+    for(int j=n-1;j>=0;j--){
+     if(i == m-1 && j==n-1) {
+        curr[j] = 1;
+        continue;
+     }
+     int ans = 0;
+     int dx[] = {0,1};
+     int dy[] = {1,0};
+     for(int k=0;k<2;k++){
+        if(isSafe(i+dx[k] , j+dy[k],m,n)){
+         if(dx[k]==0)  ans += curr[j+1];
+         else ans += next[j];
+        }
+     }
+      curr[j] = ans;
+    }
+    next = curr;
+   
+  }
+  return curr[0];
+}
+
+
 
 
     int uniquePaths(int m, int n) {
@@ -71,7 +101,10 @@ vector<vector<int>>dp(m+1,vector<int>(n+1,0));
     //    return solveMemo(0,0,m,n,dp);
 
        // Tabulation 
-       return solveTab(m,n);
+      // return solveTab(m,n);
+
+      //space optimization
+      return solveSO(m,n);
 
     }
 };
