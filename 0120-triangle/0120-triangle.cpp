59d6151a409b9478d02123602d30c1ebf69cfe02
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-22 15:32:09 +0530

    Time: 2 ms (16.18%), Space: 13.3 MB (51.91%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -24,6 +24,29 @@ int solveMemo(int i , int j , vector<vector<int>>& triangle, vector<vector<int>>
    return dp[i][j];
 }
 
+// tabulation 
+int solveTab(vector<vector<int>>& triangle){
+    int n = triangle.size();
+    vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MAX));
+
+    //Base case
+    for(int i=0;i<n;i++){
+        dp[n-1][i] = triangle[n-1][i];
+    }
+
+    for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+
+             int ans1 = dp[i+1][j];
+             int ans2 = dp[i+1][j+1];
+
+             dp[i][j] =  triangle[i][j] + min(ans1,ans2);
+        }
+    }
+    return dp[0][0];
+}
+
+
 
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
@@ -31,7 +54,11 @@ int solveMemo(int i , int j , vector<vector<int>>& triangle, vector<vector<int>>
         //return solveRec(0,0,triangle);
 
        //Memoization 
-        vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MAX));
-        return solveMemo(0,0,triangle,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(n+1,INT_MAX));
+        // return solveMemo(0,0,triangle,dp);
+
+        //Tabulation 
+        return solveTab(triangle);
+
     }
 };
