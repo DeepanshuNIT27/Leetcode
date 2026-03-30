f9cdbfd1e6f6103a5ac1adeccaa359d7598c17a1
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-03-30 20:32:27 +0530

    Time: 272 ms (5.03%), Space: 174.8 MB (5.09%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
-// O(n) complexity soln using deque
+// O(nlogn) complexity soln using deque
     int shortestSubarray(vector<int>& nums, int k) {
         int n = nums.size();
-        deque<int>dq;
+       set<pair<long long, int>> st; 
         vector<long long>prefixSum(n+1,0);
         for(int i=0;i<n;i++){
             prefixSum[i+1] = prefixSum[i] + nums[i];
@@ -12,16 +12,16 @@ public:
         int result = n+1;
         for(int i=0;i<=n;i++){
          
-         while(!dq.empty() && prefixSum[i] - prefixSum[dq.front()] >= k){
-               result = min(result , i-dq.front());
-               dq.pop_front();
-        }
+        while (!st.empty()) {
+                auto it = st.begin();
+                if (prefixSum[i] - it->first >= k) {
+                    result = min(result, i - it->second);
+                    st.erase(it);
+                } else break;
+            }
 
-        while(!dq.empty() && prefixSum[i]<=prefixSum[dq.back()]){
-            dq.pop_back();
-        }
-
-        dq.push_back(i);
+      
+        st.insert({prefixSum[i], i});
     }
 
     return result == n+1 ? -1 : result;
