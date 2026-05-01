57b67da3b7149c6ab00caed40a2830e9152c90a6
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-01 13:02:40 +0530

    Time: 187 ms (14.08%), Space: 111.3 MB (13.94%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -13,10 +13,30 @@ bool solveRec(int i , int sum ,vector<int>& nums){
 
     return inc || exc ;
 }
+
+// Memoization 
+bool solveMemo(int i , int sum ,vector<int>& nums,vector<vector<int>>&dp){
+    //Base case
+    if(sum<0) return false;
+    if(i>=nums.size()){
+        if(sum == 0) return true;
+        else return false;
+    }
+   if(dp[i][sum]!=-1) return dp[i][sum];
+    bool inc = solveMemo(i+1,sum-nums[i],nums,dp);
+    bool exc = solveMemo(i+1,sum,nums,dp);
+
+    return dp[i][sum] = inc || exc ;
+}
+
+
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(),nums.end(),0);
         if(sum&1) return false;
 
-        return solveRec(0,sum/2,nums);
+       // return solveRec(0,sum/2,nums);
+       int d = sum/2;
+       vector<vector<int>>dp(nums.size()+1 , vector<int>(d+1,-1));
+       return solveMemo(0,d,nums,dp);
     }
 };
