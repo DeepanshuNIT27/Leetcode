1d9cc79e32cb0e5ab0b541adf77c722486aa89d3
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-06 11:28:01 +0530

    Time: 64 ms (9.29%), Space: 53.9 MB (8.41%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,98 +1,53 @@
 class Solution {
 public:
-//recursion ka soln
-// int solve(vector<int>& coins, int amount){
-//     if(amount == 0){
-//         return 0;
-//     }
-//     int mincoinsans= INT_MAX;
-//     for(int i=0 ;i <coins.size();i++){
-//         int coin= coins[i];
-//         if(coin<=amount){
-//          int recursionans = solve(coins, amount-coin);
-//          if(recursionans!= INT_MAX){
-//             int coinsused = 1+ recursionans;
-//             mincoinsans = min(mincoinsans,coinsused);
-//          }
-//         }
 
-//     }
-//     return mincoinsans;
-// }
+//RECURSION 
+int solveRec(int i , int amount , vector<int>& coins ){
 
+   if(amount == 0) return 0;
+   if(i>= coins.size() || amount<0) return INT_MAX;
 
-//1.d dp hai
-// int solveMemo(vector<int>& coins, int amount, vector<int>&dp){
-//     if(amount == 0){
-//         return 0;
-//     }
-//    if(dp[amount]!=-1) return dp[amount];
-//     int mincoinsans= INT_MAX;
-//     for(int i=0 ;i <coins.size();i++){
-//         int coin= coins[i];
-//         if(coin<=amount){
-//          int recursionans = solveMemo(coins, amount-coin,dp);
-//          if(recursionans!= INT_MAX){
-//             int coinsused = 1+ recursionans;
-//        //step->2
-//             mincoinsans= min(mincoinsans,coinsused);
-//          }
-//         }
+    int inc = solveRec(i,amount-coins[i],coins);
+    if(inc!=INT_MAX) inc += 1;
+    int exc = solveRec(i+1,amount,coins);
 
-//     }
-//     dp[amount] = mincoinsans;
-//     return dp[amount];
-// }
+    return min(inc,exc);
 
-//2.D dp
-int solveTabulation(vector<int>& coins, int amount){
-    vector<int>dp(amount+1,-1);
-    dp[0] = 0;
-    // amount -> n->0
-    //reverse 0->n
-    for(int amt = 1; amt<=amount;amt++){
-        int mincoinsans= INT_MAX;
-    for(int i=0 ;i <coins.size();i++){
-        int coin= coins[i];
-        if(coin<=amt){
-         int recursionans = dp[amt-coin];
-         if(recursionans!= INT_MAX){
-            int coinsused = 1+ recursionans;
-            mincoinsans= min(mincoinsans,coinsused);
-         }
-    }
-    }
-     dp[amt] = mincoinsans;
-    }
-    
-    return dp[amount];
 }
-    int coinChange(vector<int>& coins, int amount) {
-        // int ans = solve(coins,amount);
-        // if(ans==INT_MAX){
-        //     return -1;
-        // }
-        // else
-        // return ans;
-        //step ->1,
-        //top-down approach
-    //     int n = coins.size();
-    //     vector<int>dp(amount+1,-1);
-    //    int ans = solveMemo(coins,amount,dp);
-    //     if(ans==INT_MAX){
-    //         return -1;
-    //     }
-    //     else
-    //     return ans;
-    // }
-    // tabulation method
-    int n = coins.size();
-       int ans = solveTabulation(coins,amount);
-        if(ans==INT_MAX){
-            return -1;
-        }
-        else
-        return ans;
-    }
 
+//MEMOIZATION 
+
+int solveMemo(int i , int amount , vector<int>& coins ,vector<vector<int>>&dp){
+
+   if(amount == 0) return 0;
+   if(i>= coins.size() || amount<0) return INT_MAX;
+
+   if(dp[i][amount]!=-1) return dp[i][amount];
+
+    int inc = solveMemo(i,amount-coins[i],coins,dp);
+    if(inc!=INT_MAX) inc += 1;
+    int exc = solveMemo(i+1,amount,coins,dp);
+
+    return  dp[i][amount] =  min(inc,exc);
+
+}
+
+
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        //RECURSION 
+    //   int ans  =  solveRec(0,amount,coins);
+
+    //   if(ans==INT_MAX) return -1;
+    //   return ans;
+
+      // Memoization 
+
+      vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        int ans  =  solveMemo(0,amount,coins,dp);
+
+      if(ans==INT_MAX) return -1;
+      return ans;
+
+    }
 };
