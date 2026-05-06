715a4549cd320b92f1b1ea7a60b78539e686aee6
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-06 11:43:38 +0530

    Time: 59 ms (14.05%), Space: 52.9 MB (13.54%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -32,6 +32,28 @@ int solveMemo(int i , int amount , vector<int>& coins ,vector<vector<int>>&dp){
 
 }
 
+// Tabulation 
+
+int solveTab(vector<int>& coins, int amount){
+
+    int n = coins.size();
+    vector<vector<int>>dp(n+1,vector<int>(amount+1,INT_MAX));
+    for(int i = 0; i <= n; i++) dp[i][0] = 0;
+
+    for(int i=n-1;i>=0;i--){
+        for(int j =0; j<=amount;j++){
+          
+          int inc = INT_MAX;
+           if(j - coins[i] >=0) inc = dp[i][j - coins[i]];
+            if(inc!=INT_MAX) inc += 1;
+            int exc = dp[i+1][j];
+
+            dp[i][j] = min(inc,exc);
+        }
+    }
+    return dp[0][amount];
+}
+
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
@@ -43,8 +65,14 @@ int solveMemo(int i , int amount , vector<int>& coins ,vector<vector<int>>&dp){
 
       // Memoization 
 
-      vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans  =  solveMemo(0,amount,coins,dp);
+    //   vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+    //     int ans  =  solveMemo(0,amount,coins,dp);
+
+    //   if(ans==INT_MAX) return -1;
+    //   return ans;
+
+    // Tabulation 
+     int ans  =  solveTab(coins,amount);
 
       if(ans==INT_MAX) return -1;
       return ans;
