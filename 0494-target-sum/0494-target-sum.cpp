297993a915bb753e0a5ec1bf2a54e0a019d32c91
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-06 12:52:37 +0530

    Time: 12 ms (53.63%), Space: 26.4 MB (19.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
 
+//RECURSION 
 int solveRec(int i , int sum , int &target , vector<int>& nums){
 
     if(i>=nums.size()) {
@@ -13,11 +14,38 @@ int solveRec(int i , int sum , int &target , vector<int>& nums){
 
     return add + sub ;
 }
+
+//MEMOIZATION 
+int solveMemo(int i , int sum , int &target , vector<int>& nums,vector<vector<int>>&dp , int offset){
+
+    if(i>=nums.size()) {
+        if(sum == target) return 1;
+        return 0;
+    }
+    if(dp[i][sum+offset]!=-1) return dp[i][sum+offset];
+
+    int add = solveMemo(i+1, sum+nums[i], target , nums,dp,offset);
+    int sub = solveMemo(i+1,sum - nums[i] , target, nums,dp,offset);
+
+    return  dp[i][sum+offset]  = add + sub ;
+}
+
+
     int findTargetSumWays(vector<int>& nums, int target) {
 
         int n = nums.size();
+        int s = accumulate(nums.begin(),nums.end(),0);
+        int offset = s;
         //RECURSION 
-        return solveRec(0,0,target,nums);
+       // return solveRec(0,0,target,nums);
+
+       //Memoization 
+     
+       vector<vector<int>>dp(n+1,vector<int>(2*s + 1 ,-1));
+       return solveMemo(0,0,target,nums,dp,offset);
+
+
+
 
         
     }
