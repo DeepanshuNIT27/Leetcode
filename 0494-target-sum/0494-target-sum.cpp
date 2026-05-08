b060bee4bc3b02f6a33ad0abf7836e699bc6ce32
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-08 07:21:40 +0530

    Time: 22 ms (42.21%), Space: 25.8 MB (23.28%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -30,19 +30,49 @@ int solveMemo(int i , int sum , int &target , vector<int>& nums,vector<vector<in
     return  dp[i][sum+offset]  = add + sub ;
 }
 
+// Tabulation 
+
+int solveTab(vector<int>& nums , int target , vector<vector<int>>&dp , int &offset ){
+    int n = nums.size();
+  int S = offset;
+   dp[n][target+offset] = 1;
+    for(int i=n-1;i>=0;i--){
+        for(int sum = -S ; sum<=S;sum++){
+
+            int add  = 0 ;
+            int sub  = 0;
+
+          if(sum + nums[i] <= S && sum + nums[i] >= -S)  add = dp[i+1][sum+nums[i]+offset];
+           if(sum - nums[i] <= S && sum - nums[i] >= -S)sub = dp[i+1][sum-nums[i]+offset];
+
+            dp[i][sum+offset]  = add + sub ;
+
+        }
+    }
+
+    return dp[0][offset];
+}
+
+
 
     int findTargetSumWays(vector<int>& nums, int target) {
 
         int n = nums.size();
         int s = accumulate(nums.begin(),nums.end(),0);
+        if(abs(target) > s) return 0;
         int offset = s;
         //RECURSION 
        // return solveRec(0,0,target,nums);
 
        //Memoization 
      
-       vector<vector<int>>dp(n+1,vector<int>(2*s + 1 ,-1));
-       return solveMemo(0,0,target,nums,dp,offset);
+    //    vector<vector<int>>dp(n+1,vector<int>(2*s + 1 ,-1));
+    //    return solveMemo(0,0,target,nums,dp,offset);
+
+    // Tabulation 
+     
+     vector<vector<int>>dp(n+1,vector<int>(2*s + 1 ,0));
+     return solveTab(nums,target, dp , offset);
 
 
 
