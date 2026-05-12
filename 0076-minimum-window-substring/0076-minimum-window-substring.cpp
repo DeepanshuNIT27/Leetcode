29489d8c45144de37f3dfc451b1942fb9bba48c1
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-12 11:07:54 +0530

    Time: 3 ms (78.63%), Space: 11.2 MB (93.54%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,43 +1,40 @@
 class Solution {
 public:
-//THIS IS MY FIRST APPROACH (26*M+N) COMPLEXITY
+//OPTIMAL SOLUTION O(M+N) WHERE CHECKING HASH IS DONE IN CONSTANT TIME
+// NOT ITERATING T AGAIN AND AGAIN
     string minWindow(string s, string t) {
-        if(t.size()>s.size()) return "";
-        unordered_map<char,int>mp1;
-        unordered_map<char,int>mp2;
-        for(auto&it:t) mp2[it]++;
+     unordered_map<char,int>need;
+     int n = s.size();
+     int m = t.size();
+     if(m>n)return "";
+     int start = -1;
+     int minlength = INT_MAX;
+     int ans = 0;
+     int i =0 ;
+     int j = 0;
+     for(auto&it:t) need[it]++;
 
-        int i = 0;
-        int j = 0;
-        int n = s.size();
-        int start = 0;
-        int minlength = INT_MAX;
+     while(j<n){
 
-        while(j<n){
+        char ch = s[j];
+     if(need[ch]>=1) ans++;
+     need[ch]--;
 
-            mp1[s[j]]++;
+      while(ans==m && i<=j){
 
-            while(i<=j){
-                bool flag = true;
-                for(auto&it:mp2){
-                    if(it.second>mp1[it.first]){
-                        flag = false ;
-                        break;
-                    }
-                }
-                if(!flag) break;
-                if(j-i+1 < minlength){
-                    start = i;
-                    minlength = j-i+1;
-                }
-              if(mp1[s[i]]==1) mp1.erase(s[i]);
-              else mp1[s[i]]--;
-              i++;
+       if(j-i+1<minlength) {
+        start = i;
+        minlength = j-i+1;
+       }
+       need[s[i]]++;
+       if(need[s[i]]>=1) ans--;
+      
+      i++;
+      }
+      j++;
 
-            }
-            j++;
-        }
-        if(minlength==INT_MAX) return "";
-return s.substr(start,minlength);
+     }
+     if(minlength==INT_MAX) return "";
+     return s.substr(start,minlength);
     }
 };
