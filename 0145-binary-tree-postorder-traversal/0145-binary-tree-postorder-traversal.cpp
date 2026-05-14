f0b4de8c0d627f5a9ec0bde1fa90655d18fde7e3
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-14 09:11:24 +0530

    Time: 0 ms (100%), Space: 10.9 MB (92.33%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,27 +11,45 @@
  */
 class Solution {
 public:
-   // ITERATIVE POST ORDER USING TWO STACK
     vector<int> postorderTraversal(TreeNode* root) {
-        vector<int>postorder;
-        if(root == nullptr) return postorder ;
-
-        stack<TreeNode*>st1,st2;
-        st1.push(root);
-        while(!st1.empty()){
-            root  = st1.top();
-            st1.pop();
-            st2.push(root);
+        //POSTORDER TRAVSERSAL USING SINGLE STACK;
         
-        if(root->left!=nullptr) st1.push(root->left);
-        if(root->right!=nullptr) st1.push(root->right);
+        vector<int> post;
+
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        TreeNode* temp;
+
+        while (curr != nullptr || !st.empty()) {
+
+            if (curr != nullptr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else {
+
+                temp = st.top()->right;
+
+                if (temp == nullptr) {
+
+                    temp = st.top();
+                    st.pop();
+
+                    post.push_back(temp->val);
+
+                    while (!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+
+                        post.push_back(temp->val);
+                    }
+                }
+                else {
+                    curr = temp;
+                }
+            }
         }
 
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
-        }
-
-       return postorder; 
+        return post;
     }
 };
