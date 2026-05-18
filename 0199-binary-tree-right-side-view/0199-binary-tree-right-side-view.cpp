7287b780635be1e4f725bfc743c0930441a13e35
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-18 13:53:25 +0530

    Time: 0 ms (100%), Space: 14.7 MB (95.65%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,29 +11,18 @@
  */
 class Solution {
 public:
-// LEVEL ORDER TRAVERSAL CODE 
+
+void rightview(TreeNode* root, int level ,  vector<int>&ans){
+    if(root == nullptr) return ;
+
+   if(ans.size()==level) ans.push_back(root->val);
+   rightview(root->right, level+1,ans);
+   rightview(root->left , level+1,ans);
+}
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
         if(root==nullptr) return ans;
-        queue<pair<TreeNode*,int>>q;
-        q.push({root,0});
-        map<int,int>mp;
-
-        while(!q.empty()){
-
-            auto node = q.front().first;
-            auto row = q.front().second;
-            q.pop();
-
-            if(mp.find(row)==mp.end()) mp[row] = node->val;
-
-            if(node->right!=nullptr) q.push({node->right,row+1});
-            if(node->left!=nullptr) q.push({node->left,row+1});
-          
-
-        }
-        for(auto&it:mp) ans.push_back(it.second);
+        rightview(root,0,ans);
         return ans;
-        
     }
 };
