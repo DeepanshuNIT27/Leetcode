eb68f8d40d7cd29d2a246053b5ea2ed8a06b4ef1
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-19 18:47:35 +0530

    Time: 0 ms (100%), Space: 9.2 MB (46.36%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,7 +1,60 @@
+#define ll long long int 
 class Solution {
 public:
+    const ll RADIX_1 = 26;
+    const ll MOD_1 = 1000000007;
+    const ll RADIX_2 = 27;
+    const ll MOD_2 = 1000000033;
+
+    pair<ll,ll>hashPair(string string , ll m){
+        ll hash1 = 0  , hash2 = 0;
+        ll factor1 = 1 , factor2 = 1;
+
+        for(ll i=m-1;i>=0;i--){
+
+            hash1 += ((string[i]-'a')*factor1)%MOD_1;
+            factor1 = (factor1 * RADIX_1) % MOD_1;
+            hash2 += ((string[i]-'a')*factor2) % MOD_2;
+            factor2 = (factor2 * RADIX_2) % MOD_2;
+        }
+        return {hash1%MOD_1 , hash2%MOD_2};
+    }
     int strStr(string haystack, string needle) {
-        int ans =haystack.find(needle);
-        return ans;
+        ll n = haystack.length() , m = needle.length();
+        if(n<m) return -1;
+
+        ll MAX_WEIGHT_1 = 1 , MAX_WEIGHT_2 = 1;
+
+        for(ll i=0; i<m;i++){
+            MAX_WEIGHT_1 = (MAX_WEIGHT_1 * RADIX_1) % MOD_1;
+            MAX_WEIGHT_2 = (MAX_WEIGHT_2 * RADIX_2) % MOD_2;
+        }
+
+        pair<ll,ll>hashNeedle = hashPair(needle, m);
+        pair<ll,ll>hashHay = {0,0};
+
+        for(ll i=0 ; i<=n-m ;i++){
+            if(i==0){
+                hashHay = hashPair(haystack,m);
+            }
+            else {
+        hashHay.first = 
+              ((hashHay.first * RADIX_1) % MOD_1
+           - ((haystack[i-1]-'a') * MAX_WEIGHT_1) % MOD_1
+         + (haystack[i+m-1]-'a') + MOD_1
+) % MOD_1;
+
+
+       hashHay.second = 
+                ((hashHay.second * RADIX_2) % MOD_2
+               - ((haystack[i-1]-'a') * MAX_WEIGHT_2) % MOD_2
+               + (haystack[i+m-1]-'a')
+               + MOD_2
+) % MOD_2;
+            }
+
+            if(hashNeedle.first == hashHay.first && hashNeedle.second == hashHay.second) return i;
+        }
+        return -1;
     }
 };
