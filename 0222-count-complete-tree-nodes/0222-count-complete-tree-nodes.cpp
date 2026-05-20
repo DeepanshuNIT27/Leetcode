8f1498e7fb2bba03315f52d8973ac753e71e088b
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-20 15:58:26 +0530

    Time: 0 ms (100%), Space: 31.3 MB (44.24%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -10,20 +10,41 @@
  * };
  */
 
-// it is using simple dfs O(N)
+ // we will use the property of the binary tree 
+ // time complexity of that sol is (log(n))^2
 class Solution {
 public:
 
-int totalNode(TreeNode* root){
+int leftheight(TreeNode* root){
 
     if(root == nullptr) return 0;
+    int count = 0;
+    while(root){
+        count++;
+        root =  root->left;
+    }
+    return count ;
+}
 
-    int leftNode = totalNode(root->left);
-    int rightNode = totalNode(root->right);
+int rightheight(TreeNode* root){
 
-    return 1 + leftNode + rightNode;
+    if(root == nullptr) return 0;
+    int count = 0;
+    while(root){
+        count++;
+        root = root->right;
+    }
+    return count;
 }
     int countNodes(TreeNode* root) {
-        return totalNode(root);
+        
+        if(root == nullptr) return 0;
+
+        int left = leftheight(root->left);
+        int right =rightheight(root->right);
+
+        if(left == right) (1<<left) - 1 ;
+
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
 };
