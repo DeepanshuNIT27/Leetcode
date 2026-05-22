4803054725ee22bef47ac67aa3014a14426fbf80
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-22 10:12:26 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.69%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,57 +9,19 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//METHOD-1 ; LAMBA METHOD
-//  class Info{
-// public:
-//  long long minval;
-//  long long maxval;
-//  bool isBst;
-//  };
-// class Solution {
-// public:
-// Info solve(TreeNode* root,bool &ans){
-//     if(root== NULL){
-//         Info temp;
-//         temp.minval = LONG_MAX;
-//         temp.maxval = LONG_MIN;
-//         temp.isBst = true;
-//         return temp;
-//     }
-//     Info leftans = solve(root->left,ans);
-//     Info rightans = solve(root->right,ans);
-//     Info currans;
-//      currans.minval = min((long long)leftans.minval,(long long)root->val);
-//     currans.maxval = max((long long)rightans.maxval,(long long)root->val);
-//    currans.isBst = (root->val> leftans.maxval && root->val < rightans.minval && leftans.isBst && rightans.isBst);
-//    if(!currans.isBst){
-//     ans = false;
-//    }
-//    return currans;
+class Solution {
+public:
+bool solve(TreeNode* root , long long maxi , long long mini){
 
-// }
-//     bool isValidBST(TreeNode* root) {
-//         bool ans = true;
-//         solve(root,ans);
-//         return ans;
-        
-//     }
-// };
-// METHOD-2 RANGE CHECK METHOD;
-class Solution{
-    public:
-    bool validate(TreeNode* root ,long long lb , long long ub){
-        if(!root) return true;
+    if(root == NULL) return true;
 
-        bool iscurrnodeinrange = root->val > lb && root->val <ub;
+    if(root->val>= maxi || root->val<=mini) return false;
 
-        bool isleftbst = validate(root->left ,lb , root->val);
-        bool isrightbst = validate(root->right ,root->val ,ub);
-        return iscurrnodeinrange &&isleftbst && isrightbst;
-    }
-    bool isValidBST(TreeNode* root){
-        long long lb = LONG_MIN;
-        long long ub = LONG_MAX;
-        return validate(root,lb,ub);
+    return solve(root->left , root->val , mini) && solve(root->right,maxi,root->val);
+}
+    bool isValidBST(TreeNode* root) {
+        long long maxi = 1e18;
+        long long mini = -1e18;
+        return solve(root,maxi,mini);
     }
 };
