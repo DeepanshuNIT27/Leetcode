95517fee7b7aa6fbb3aa898ee262e250e3368e34
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-22 10:35:04 +0530

    Time: 25 ms (41.36%), Space: 23.4 MB (91.02%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,26 +11,26 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == NULL){
-            return NULL;
-        }
-        //CASE 1 BOTH P & Q LEFT ME HO
-        if(p->val< root->val&& q->val< root->val){
-            TreeNode* leftans = lowestCommonAncestor(root->left,p,q);
-            if(leftans!=NULL){
-                return leftans;
-            }
-        }
-        // CASE 2 BOTH P & Q RIGHT SIDE ME HO
-        if(p->val>root->val&& q->val>root->val){
-         TreeNode* rightans = lowestCommonAncestor(root->right,p,q);
-         if(rightans!= NULL){
-            return rightans;
-         }
-        }
-        // CASE 3 P LEFT ME HO Q RIGHT ME HO
-        //CASE 4 P RIGHT ME HO Q LEFT ME HO
-        return root;
         
+        if(root == NULL ) return NULL;
+
+        // case -1 both are in same side left side 
+        if(p->val < root->val && q->val <root->val){
+
+            TreeNode* leftans = lowestCommonAncestor(root->left,p,q);
+
+            if(leftans!=NULL) return leftans;
+        }
+
+        // case -2 both are present at right side 
+        if(p->val> root->val && q->val > root->val){
+
+            TreeNode* rightans = lowestCommonAncestor(root->right,p,q);
+
+            if(rightans!=NULL) return rightans;
+        }
+
+        // both are diff side 
+        return root;
     }
 };
