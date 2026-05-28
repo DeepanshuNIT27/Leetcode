3e790d5cf514127fe55132133051af3e516a351f
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-28 15:30:07 +0530

    Time: 0 ms (100%), Space: 13.4 MB (40.16%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,35 +10,19 @@
  */
 class Solution {
 public:
-//RECURSIVE SOLUTION
-// ListNode*reverseList(ListNode* &prev,ListNode*&curr){
-//     //BASE CASE
-//     if(curr==NULL){
-//         return prev;
-//     }
-//     ListNode* forward = curr->next;
-//     curr->next = prev;
-//     prev= curr;
-//     curr= forward;
-//     return reverseList(prev,curr);
-// }
-//     ListNode* reverseList(ListNode* head) {
-//         ListNode*prev=NULL;
-//         ListNode*curr=head;
-//         ListNode*newHead = reverseList(prev,curr);
-//         return newHead;
-        
-//     }
-//ITERATIVE SOLUTION
-ListNode*reverseList(ListNode*head){
-    ListNode*prev=NULL;
-    ListNode*curr=head;
-    while(curr!=NULL){
-        ListNode*forward = curr->next;
-        curr->next= prev;
-        prev = curr;
-        curr= forward;
+    ListNode* reverseList(ListNode* head) {
+        if(head== NULL || head->next ==NULL) return head;
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* upcome = NULL;
+        while(curr!=NULL){
+            upcome = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = upcome;
+          
+        }
+        return prev;
     }
-    return prev;
-}
 };
