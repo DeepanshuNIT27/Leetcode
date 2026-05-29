15c7c1e3544ab1d0235e14178f31048c228268dd
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-29 17:57:46 +0530

    Time: 53 ms (77.8%), Space: 23.9 MB (54.96%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,22 +8,42 @@
  */
 class Solution {
 public:
-// BRUTE FORCE APPROACH 
-//COMPLEXITY O(N+M) TIME O(N) SPACE.
+// OPTIMIZED APPROACH  COMPLEXTY O(N+M) AND SPACE CONSTANT O(1).
+// AGAR VO INTERSECT KREGI MEANS SAME TAILSHARE KREGI DONO KA LENGTH NIKAL /LO AB JITNA LENGTH KA DIFF HAI UTNA JYADA LENGTH WALE KO AAGE BADHAO
+//AB SATH ME COMPARE KRTE CHALO JO SAME NODE AAYA VO ANS HOGA
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         
-        unordered_map<ListNode*,bool>mp;
+        int count1 = 0;
+        int count2 = 0;
 
         ListNode* curr = headA;
         while(curr!=NULL){
-            mp[curr] = true;
+            count1++;
             curr = curr->next;
         }
-
         curr = headB;
         while(curr!=NULL){
-            if(mp.find(curr)!=mp.end()) return curr;
-               curr = curr->next;
+            count2++;
+            curr = curr->next;
+        }
+        int d = abs(count1-count2);
+        ListNode* startA = headA;
+        ListNode* startB = headB;
+        if(max(count1,count2)== count1){
+            for(int i=0;i<d;i++){
+              startA = startA->next;
+            }
+        }
+        else {
+             for(int i=0;i<d;i++){
+              startB = startB->next;
+            }
+        }
+        while(startA!=NULL && startB!=NULL){
+
+            if(startA == startB) return startA;
+            startA = startA->next;
+            startB = startB->next;
         }
         return NULL;
     }
