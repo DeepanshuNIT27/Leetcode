61b78544b59473babb901cd8103079aa9ded79a0
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-29 16:03:46 +0530

    Time: 15 ms (63.66%), Space: 57 MB (89.36%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -8,27 +8,61 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //OPTIMIZED APPROACH HERE WILL DO MERGE SORT 
+ // COMPLEXITY O(N*LOG(N)) AND SPACE LOG(N) AUXALIARY CALL STACK. 
 class Solution {
 public:
-//BRUTE FORCE SOLUTION COMPLEXITY O(N) FOR TIME &SPACE BOTH.
+
+// MERGE OPERATION 
+ListNode* merge( ListNode* left, ListNode* right){
+
+    ListNode dummy(-1);
+    ListNode* tail = &dummy;
+
+    while(left && right){
+
+        if(left->val < right->val){
+
+            tail->next = left;
+            left = left->next;
+        }
+
+        else{
+            tail->next = right;
+            right = right->next;
+        }
+        tail = tail->next;
+    }
+    if(left) tail->next = left;
+    else tail->next = right;
+
+    return dummy.next;
+
+    
+}
     ListNode* sortList(ListNode* head) {
-        vector<int>v;
-        ListNode* curr = head;
+        
+        if(head == NULL || head->next == NULL) return head;
 
-        while(curr!=NULL){
-            v.push_back(curr->val);
-            curr = curr->next;
+        // find middle 
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast!=NULL && fast->next!=NULL){
+
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        sort(v.begin(),v.end());
-        int i = 0;
+        ListNode* right = slow->next;
+        slow->next = NULL;
 
-        curr = head;
-        while(curr!= NULL){
-            curr->val =v[i];
-            i++;
-            curr = curr->next;
-        }
-        return head;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        // merge them 
+        return merge(left , right);
     }
 };
