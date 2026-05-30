fa2abd4bbdc666d39622a7f560296d2dd83136d8
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-05-30 10:30:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (92.18%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,17 +1,10 @@
 class Solution {
 public:
-// METHOD-1 COMPLEXITY O(1) FOR BOTH
+// COMMON SOLN BY OBSERVATION 
     int minBitFlips(int start, int goal) {
-
-        int cnt = 0;
-        for (int i = 0; i < 32; i++) {
-
-            bool ans1 = (((start >> i) & 1) != 0);
-            bool ans2 = (((goal >> i) & 1) != 0);
-
-            if ((ans1 && !ans2) || (!ans1 && ans2))
-                cnt++;
-        }
+        
+        int d = start^goal;
+        int cnt = __builtin_popcount(d);
         return cnt;
     }
 };
