32eb37e3e6d0ca6aa2d7846f7900ea432354527d
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-06 08:27:23 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.27%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,57 @@
 class Solution {
 public:
+//WITHOUT STL APNE SE SEARCH INDEX LIKHO
+int firstOccurance(vector<int>& nums, int target){
+
+    int n = nums.size();
+    int  s = 0;
+    int e  = n-1;
+    int ans = -1;
+
+    while(s<=e){
+           
+           int mid = (s+e)/2;
+           if(nums[mid] == target){
+             ans = mid;
+             e = mid - 1;
+           }
+           else if(nums[mid]>target){
+            e  = mid - 1;
+           }
+           else{
+             s = mid +1 ;
+           }
+    }
+    return ans;
+}
+
+int lastOccurance(vector<int>& nums, int target){
+        int n = nums.size();
+        int  s = 0;
+        int e  = n-1;
+        int ans = -1;
+
+        while(s<=e){
+           
+           int mid = (s+e)/2;
+           if(nums[mid] == target){
+             ans = mid;
+             s= mid + 1;
+           }
+           else if(nums[mid]>target){
+            e  = mid - 1;
+           }
+           else{
+             s = mid +1 ;
+           }
+    }
+    return ans;
+}
     vector<int> searchRange(vector<int>& nums, int target) {
-           vector<int>ans;
-           if(nums.size() == 0) return {-1,-1};
-           int n = nums.size();
-       
-        int start = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int upper = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-      
-        if( start== n || nums[start]!=target) {
-            ans.push_back(-1);
-            ans.push_back(-1);
-        }
-       else{
-          ans.push_back(start);
-          ans.push_back(upper-1);
-       }
-       return ans;
+        
+        int a = firstOccurance(nums,target);
+        int b = lastOccurance(nums,target);
+
+        return {a,b};
     }
 };
