a8fd690c1e7fa25fa78715a17b2e37a9a92c88a5
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-06 08:08:06 +0530

    Time: 0 ms (100%), Space: 17.5 MB (88.23%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,72 +1,21 @@
- class Solution {
+class Solution {
 public:
-int firstoccurance(vector<int>&nums,int target){
-  int s = 0;
-          int n = nums.size();
-    int e = n - 1;
-    int mid;
-    int ans1 =-1;
-    mid = s + (e - s) / 2;
-    while (s <= e)
-    {
-        if (nums[mid] == target)
-        {
-            ans1= mid;
-            e = mid - 1;
-        }
-        if (nums[mid] < target)
-        {
-            s = mid + 1;
-        }
-        if (nums[mid] > target)
-        {
-            e = mid - 1;
-        }
-        mid = s+(e-s)/2;
-
-        
-    }
-    return ans1;
-}
-int lastoccurance(vector<int>&nums,int target){
-  int s = 0;
-          int n = nums.size();
-    int e = n - 1;
-    int mid;
-    int ans2 =-1;
-    mid = s + (e - s) / 2;
-    while (s <= e)
-    {
-        if (nums[mid] == target)
-        {
-            ans2= mid;
-            s = mid + 1;
-        }
-        if (nums[mid] < target)
-        {
-            s = mid + 1;
-        }
-        if (nums[mid] > target)
-        {
-            e = mid - 1;
-        }
-        mid = s+(e-s)/2;
-
-        
-    }
-    return ans2;
-}
     vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int>arr(2);
-      int a=  firstoccurance(nums, target);
-     int b =   lastoccurance(nums, target);
-
-arr[0]=a;
-arr[1]=b;
-
-return arr;
-    
-
-}
- };      
-
+           vector<int>ans;
+           if(nums.size() == 0) return {-1,-1};
+           int n = nums.size();
+       
+        int start = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+        int upper = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+      
+        if( start== n || nums[start]!=target) {
+            ans.push_back(-1);
+            ans.push_back(-1);
+        }
+       else{
+          ans.push_back(start);
+          ans.push_back(upper-1);
+       }
+       return ans;
+    }
+};
