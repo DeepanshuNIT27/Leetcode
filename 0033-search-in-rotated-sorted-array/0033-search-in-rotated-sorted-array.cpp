f254d2f67e5c2e3e3bbd68f8f23dfcc0f5ab5f8e
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-06 09:33:15 +0530

    Time: 0 ms (100%), Space: 15.2 MB (66.7%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,62 +1,37 @@
 class Solution {
 public:
-int getPivotIndex(vector<int>&nums){
-    int s =0;
-    int n = nums.size();
-    int e = n-1;
-    int mid = s+(e-s)/2;
-    while(s<=e){
-        if(s==e){
-            return s;
-        }
-        if( mid+1<=n && nums[mid]>nums[mid+1]){
-            return mid;
-        }
-       
-            
-        if(nums[mid]<nums[s]){
-            e = mid-1;
-        }
-        else{
-            s= mid+1;
-        }
-        mid = s+(e-s)/2;
-    }
-    return -1;
-}
+// USE OBSERVATION 
+    int search(vector<int>& nums, int target) {
+        int  n  = nums.size();
+        int  s  = 0;
+        int  e  = n-1;
 
-int binarySearch(int s ,int e ,int target ,vector<int>&nums){
-int mid = s+(e-s)/2;
-while(s<=e){
-if(nums[mid]==target){
-    return mid;
-}
- else if( nums[mid]>target){
-    e = mid-1;
+        while(s<=e){
 
-}
- else if (nums[mid]<target){
-    s=mid+1;
-}
-mid = s+(e-s)/2;
+            int mid = s + (e-s)/2;
 
-}
-return -1;
+            if(nums[mid] == target) return mid;
 
+            //left sorted
+           if(nums[s]<= nums[mid]){
 
-}
-    int search(vector<int>&nums, int target) {
-        int pivotIndex= getPivotIndex(nums);
-        int n = nums.size();
-        
-
-        if(target>=nums[0]&& target<=nums[pivotIndex]){
-           int ans1 = binarySearch(0,pivotIndex,target,nums);
-            return ans1;
-        }
-        else{
-         int ans2 =binarySearch(pivotIndex+1,n-1,target,nums);
-            return ans2;
+            if(nums[s]<= target && target<=nums[mid] ) {
+                e  = mid-1;
+            }
+            else {
+                 s = mid + 1;
+            }
+              
+           }
+           //right sorted
+           else{
+              if(nums[mid]<=target && target<=nums[e]){
+                s = mid+1;
+              }
+              else {
+                e = mid-1;
+              }
+           }
         }
         return -1;
     }
