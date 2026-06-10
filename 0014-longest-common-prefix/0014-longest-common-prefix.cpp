e555be2d64afb31d8832bcf183981b2f07c4de06
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-10 15:59:40 +0530

    Time: 0 ms (100%), Space: 11.8 MB (79.84%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,56 +1,18 @@
-class TrieNode{
- public:
- char data;
- unordered_map<char,TrieNode*>children;
- bool isTerminal;
- TrieNode(char value){
-    data = value;
-    isTerminal = false;
- }
-};
-  void insertWord(TrieNode* root , string word){
-         if(word.length()==0){
-            root->isTerminal = true;
-            return ;
-     }
-        char ch = word[0];
-        TrieNode* child;
-        if(root->children.find(ch)!=root->children.end()){
-            child = root->children[ch];
-        }
-        else{
-            child = new TrieNode(ch);
-            root->children[ch] = child;
-        }
-        insertWord(child,word.substr(1));
-    }
-   
-void findlcp(TrieNode* root , string &ans){
-    if(root->isTerminal == true){
-        return ;
-    }
-    TrieNode* child;
-    if(root->children.size() == 1){
-        for(auto i: root->children){
-            char ch = i.first;
-            ans.push_back(ch);
-            child = i.second;
-        }
-    }
-    else{
-        return ;
-    }
-    findlcp(child,ans);
-}
 class Solution {
 public:
- TrieNode* root = new TrieNode('-');
     string longestCommonPrefix(vector<string>& strs) {
-        for(auto i:strs){
-            insertWord(root,i);
+        
+        int n = strs.size();
+        string ans = strs[0];
+        for(int i=1;i<n;i++){
+
+            int j = 0;
+
+            while(j<strs[i].size() && j<ans.size()  && strs[i][j]==ans[j]) j++;
+
+            ans = ans.substr(0,j);
+            if(ans.empty()) return ans;
         }
-        string ans ="";
-       findlcp(root,ans);
         return ans;
     }
 };
