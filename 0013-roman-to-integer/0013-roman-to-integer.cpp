d72dc2c4296bb0670767471a766ab514763a5706
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-11 18:16:32 +0530

    Time: 11 ms (27.36%), Space: 13.4 MB (18.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int>mp;
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
 
-        int n = s.size();
-        int ans = 0;
-        for(int i=0;i<n;i++){
+        unordered_map<char,int>mp = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+    };
 
-            if(i+1<n &&(s[i]=='I' && s[i+1]=='V') ||(s[i]=='I' && s[i+1]=='X')
-            ||(s[i]=='X' && s[i+1]=='L') || (s[i]=='X' && s[i+1]=='C')
-            || (s[i]=='C' && s[i+1]=='D') ||(s[i]=='C' && s[i+1]=='M'))
-            {
-                  ans += - mp[s[i]];
-                  ans +=  mp[s[i+1]];
-                  i++;
-            }
-            else{
-                ans += mp[s[i]];
-            }
+    int n = s.size();
+    int ans  = 0;
+
+    for(int i=0;i<n;i++){
+
+        if(i+1<n && mp[s[i]] < mp[s[i+1]]){
+            ans += -mp[s[i]];
         }
-        return ans;
+        else{
+            ans += mp[s[i]];
+        }
+    }
+    return ans ;
     }
 };
