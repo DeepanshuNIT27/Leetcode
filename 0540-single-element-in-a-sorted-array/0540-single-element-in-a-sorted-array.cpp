03252a91dd2f94cfffb9b949b33d2494c491d4ff
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-17 18:29:21 +0530

    Time: 0 ms (100%), Space: 26.2 MB (33.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,48 +1,28 @@
 class Solution {
 public:
-    int singleNonDuplicate(vector<int>& nums) {
-        int s = 0;
-        int n = nums.size();
-        int e = n-1;
-        int mid = s+(e-s)/2;
-        while(s<=e){
-            if(s==e){
-                return nums[s];
-            }
-            int currentvalue =nums[mid];
-            int leftvalue= -1; 
-            if(mid-1>=0){
-                leftvalue =nums[mid-1];
-            }
-            int rightvalue =-1;
-            if( mid+1<n){
-                rightvalue= nums[mid+1];
-            }
-            if(currentvalue!=leftvalue &&  currentvalue!=rightvalue){
-                return currentvalue;
-            }
-            if(currentvalue==leftvalue && currentvalue != rightvalue){
-                int firstpair = mid-1;
-                if(firstpair&1){
-                    e= mid-1;
-                }
-                else{
-                    s= mid+1;
-                }
-            }
-            if(currentvalue==rightvalue && currentvalue!=leftvalue){
-                int firstpair= mid;
-                if(firstpair&1){
-                    e = mid-1;
-                }
-                else{
-                    s= mid+1;
-                }
-            }
-            mid = s+(e-s)/2;
+//OBSERVATION AGR SINGLE TIME AANE WALA ELEMENT NI AAYA RHEGA TB TK 
+//SBKA FIRST OCC . EVEN INDEX PE HOGA .
 
-        }
-        return -1;
+// TIME COMPLEXITY - O(LOG N) TIME O(1) SPACE .
+    int singleNonDuplicate(vector<int>& nums) {
         
+        int n  = nums.size();
+        int s = 0;
+        int e = n-1;
+
+        while(s<e){
+
+            int mid  = s + (e-s)/2;
+
+            if(mid&1) mid--;
+
+            if(nums[mid] == nums[mid+1]) {
+                s = mid + 2;
+        }
+        else {
+            e = mid;
+        }
+        }
+        return nums[s];
     }
 };
