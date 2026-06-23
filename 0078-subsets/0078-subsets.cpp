af6c1c92fbfc6116063fbb6fe9fd18ac0ac5b933
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-23 19:13:03 +0530

    Time: 0 ms (100%), Space: 9.9 MB (59.19%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-     vector<vector<int>>ans;
+//BACKTRACKING SOLN COMPLEXITY O(2^N*N).
+//BRUTE FORCE SOLUTION 
 
-     int num = nums.size();
-     int n = (1<<num);
-
-     for(int i=0;i<n;i++){
-        vector<int>temp;
-        for(int j=0;j<num;j++){
-            if((i>>j)&1 !=0) {
-                temp.push_back(nums[num-j-1]);
-            }
-        }
+void solve(int i , int n ,vector<int>& nums, vector<int>&temp ,vector<vector<int>>&ans ){
+    
+    if(i == n){
         ans.push_back(temp);
-     }
+        return ;
+    }
+    //include
+    temp.push_back(nums[i]);
+    solve(i+1,n,nums,temp,ans);
+    temp.pop_back();
 
-return ans ;
+    //exclude
+    solve(i+1,n,nums,temp,ans);
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int index = 0;
+        int n = nums.size();
+
+        solve(index,n,nums,temp,ans);
+        return ans;
     }
 };
