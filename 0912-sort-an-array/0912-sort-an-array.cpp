d51aef7b6f032dc2e93c90d4071e28dfdd7e70cd
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-24 22:49:29 +0530

    Time: 632 ms (28.81%), Space: 295.5 MB (36.52%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+//MERGE SORT 
+//COMPLEXITY O(N*LOGN) TIME AND O(N) SPACE .
+void merge(vector<int>& nums , int low , int mid, int high){
+    vector<int>temp;
+
+    int i =low ; int j= mid+1;
+    while(i<=mid && j<=high){
+
+        if(nums[i]<=nums[j]){
+            temp.push_back(nums[i]);
+            i++;
+        }
+        else {
+            temp.push_back(nums[j]);
+            j++;
+        }
+    }
+    while(i<=mid){
+        temp.push_back(nums[i]);
+        i++;
+    }
+    while(j<=high){
+        temp.push_back(nums[j]);
+        j++;
+    }
+   for(int i=low;i<=high;i++){
+    nums[i] = temp[i-low];
+   }
+}
+void mergeSort(vector<int>& nums , int low , int high){
+
+    if(low>=high) return ;
+
+    int mid = (low + high)/2;
+    mergeSort(nums,low,mid);
+    mergeSort(nums,mid+1,high);
+    merge(nums,low,mid,high);
+}
+    vector<int> sortArray(vector<int>& nums) {
+
+        int n = nums.size()-1; 
+         mergeSort(nums,0,n);
+         return nums;
+
+    }
+};
