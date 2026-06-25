c018e2e4e7e7943e6f77f70efe2cadd4c1eb200f
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-25 09:43:35 +0530

    Time: 1 ms (19.3%), Space: 9.1 MB (89.19%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,38 +1,26 @@
 class Solution {
 public:
 
-   void solve( string &output,unordered_map<char,string>&mapping, vector<string>&ans,string digits,int i){
-    //BASE CASE
-    if(i>= digits.size()){
-        if(output.size()>0){
-        ans.push_back(output);
+void solveRec(int i , string digits, string&temp,vector<string>&ans,unordered_map<char,string>&mp){
+
+    if(i == digits.size()){
+        ans.push_back(temp);
+        return ;
     }
-    return ;
+    
+    for(auto&it : mp[digits[i]]){
+
+        temp += it;
+        solveRec(i+1,digits,temp,ans,mp);
+        temp.pop_back();
     }
-    //1 CASE HM BAKI RECURSION
-    char digit = digits[i];
-    string mappedstring = mapping[digit];
-    for(auto & ch:mappedstring){
-        output.push_back(ch);
-        solve(output,mapping,ans,digits,i+1);
-        output.pop_back();
-    }
-   }
+}
     vector<string> letterCombinations(string digits) {
         vector<string>ans;
-        unordered_map<char,string>mapping;
-        mapping['2'] = "abc";
-        mapping['3'] = "def";
-        mapping['4'] = "ghi";
-        mapping['5'] = "jkl";
-        mapping['6'] = "mno";
-        mapping['7'] = "pqrs";
-        mapping['8'] = "tuv";
-        mapping['9'] = "wxyz";
-        string output = "";
-        int i = 0;
-        solve(output,mapping,ans,digits,i);
+        string temp = "";
+        unordered_map<char,string>mp = {{'2',"abc"},{'3',"def"},{'4',"ghi"},{'5',"jkl"},{'6',"mno"},{'7',"pqrs"},{'8',"tuv"},{'9',"wxyz"}};
+        
+        solveRec(0,digits,temp,ans,mp);
         return ans;
-
     }
 };
