2273ee220d1ed3a9c53122b339a2227e3d5e0a5c
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-26 18:51:43 +0530

    Time: 98 ms (59.99%), Space: 103.3 MB (36.09%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -54,6 +54,36 @@ int solveMemo(int i, int flag , vector<int>& prices,int fee , vector<vector<int>
     return dp[i][flag] = ans;
 }
 
+//TABULATION
+
+int solveTab(vector<int>& prices, int fee){
+    int n = prices.size();
+    vector<vector<int>>dp(n+2,vector<int>(2,0));
+
+    for(int i=n-1;i>=0;i--){
+        for(int flag=1;flag>=0;flag--){
+              int ans = 0;
+
+        if(flag == 0 ) {
+        int ans1 = -prices[i] + dp[i+1][1];
+    
+        int ans2 = dp[i+1][flag];
+        ans = max(ans1,ans2);
+       }
+
+       else {
+        int ans1 = prices[i] - fee + dp[i+1][0];
+
+        int ans2 = dp[i+1][flag];
+
+        ans = max(ans1,ans2);
+      }
+       dp[i][flag] = ans;
+        }
+    }
+    return dp[0][0];
+}
+
 
     int maxProfit(vector<int>& prices, int fee) {
         
@@ -63,8 +93,13 @@ int solveMemo(int i, int flag , vector<int>& prices,int fee , vector<vector<int>
         //return solveRec(0,0,prices,fee);
 
         //MEMOIZATION
-       vector<vector<int>>dp(n+1,vector<int>(2,-1));
-      return solveMemo(0,0,prices,fee,dp);
+    //    vector<vector<int>>dp(n+1,vector<int>(2,-1));
+    //   return solveMemo(0,0,prices,fee,dp);
+
+
+      //TABULATION
+     return solveTab(prices,fee);
+
 
     }
 };
