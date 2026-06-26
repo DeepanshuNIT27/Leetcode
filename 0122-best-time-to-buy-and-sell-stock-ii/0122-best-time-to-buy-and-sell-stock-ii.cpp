34292aca98aa37db8cbbd3282b00d5ad48cdbf5a
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-26 09:44:17 +0530

    Time: 19 ms (5.92%), Space: 27.4 MB (16.48%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,106 +1,61 @@
 class Solution {
 public:
-//Recursion 
-int solveRec(vector<int>& prices, int i , bool flag){
-    //Base case ;
-    if(i==prices.size()) return 0;
+//RECURSION 
+int solveRec(int i , bool flag ,vector<int>& prices ){
 
-    // sell
-    int  ans1 = 0;
-    int ans2 = 0;
-    int ans3 = 0;
-  if(flag )  ans1 =  prices[i]  + solveRec(prices,i+1,false);
-    // buy 
-  if(flag == false) ans2 = -prices[i] + solveRec(prices,i+1,true);
-    // nothing to do 
-     ans3 =  0 + solveRec(prices,i+1,flag);
+    if(i == prices.size()) return 0;
+    int ans = 0; 
+    if(flag== false){
+     //buy
+     int  ans1  =   - prices[i]+ solveRec(i+1,true,prices);
+      //skip
+     int ans2 =     solveRec(i+1,flag,prices);
+      ans =   max(ans1,ans2) ;
+    }
+    else {
+        //sell
+       int  ans1  = prices[i] + solveRec(i+1,false ,prices);
+        //skip
+      int   ans2  = solveRec(i+1,flag,prices);
+       ans  =   max(ans1,ans2);
 
-    return max(max(ans1,ans2),ans3);
+    }
+    return ans;
 }
 
-//Memoization
-int  solveMemo(vector<int>& prices, int i ,int flag , vector<vector<int>>&dp){
-    //Base case ;
-    if(i==prices.size()) return 0;
-     if(dp[i][flag]!=-1) return dp[i][flag];
-    // sell
+// MEMOIZATION .
 
-      int   ans1 = 0;
-     int  ans2 = 0;
-     int ans3 = 0;
-  if(flag)  ans1 =  prices[i] + solveMemo(prices,i+1,false,dp);
-    // buy 
-  if(flag == false) ans2 = - prices[i] + solveMemo(prices,i+1,true,dp);
-    // nothing to do 
-     ans3 =  0 + solveMemo(prices,i+1,flag,dp);
+int solveMemo(int i , int flag ,vector<int>& prices, vector<vector<int>>&dp ){
 
-    dp[i][flag] = max(max(ans1,ans2),ans3);
-    return dp[i][flag];
+    if(i == prices.size()) return 0;
+    if(dp[i][flag] != -1) return dp[i][flag];
+    int ans = 0; 
+    if(flag==0){
+     //buy
+     int  ans1  =   - prices[i]+ solveMemo(i+1,1,prices,dp);
+      //skip
+     int ans2 =     solveMemo(i+1,flag,prices,dp);
+      ans =   max(ans1,ans2) ;
+    }
+    else {
+        //sell
+       int  ans1  = prices[i] + solveMemo(i+1,0,prices,dp);
+        //skip
+      int   ans2  = solveMemo(i+1,flag,prices,dp);
+       ans  =   max(ans1,ans2);
+
+    }
+    return dp[i][flag] = ans;
 }
-// Tabulation 
-int solveTab(vector<int>& prices){
-    int n = prices.size();
-    vector<vector<int>>dp(n+1,vector<int>(2,0));
-
-    for(int i=n-1;i>=0;i--){
-        for(int flag = 0;flag<=1;flag++){
-     int   ans1 = 0;
-     int  ans2 = 0;
-     int ans3 = 0;
-   if(flag)  ans1 =  prices[i] + dp[i+1][0];
-    // buy 
-   if(flag == false) ans2 = - prices[i] + dp[i+1][1];
-    // nothing to do 
-     ans3 =  0 + dp[i+1][flag];
-
-    dp[i][flag] = max(max(ans1,ans2),ans3);
-    }
-    }
-    return dp[0][0];
-
-}
-
-// space optimization
-int solveSO(vector<int>& prices){
-    int n = prices.size();
-    vector<int>curr(2,0);
-    vector<int>next(2,0);
-
-    for(int i=n-1;i>=0;i--){
-        for(int flag = 0;flag<=1;flag++){
-     int   ans1 = 0;
-     int  ans2 = 0;
-     int ans3 = 0;
-   if(flag)  ans1 =  prices[i] + next[0];
-    // buy 
-   if(flag == false) ans2 = - prices[i] + next[1];
-    // nothing to do 
-     ans3 =  0 + next[flag];
-
-    curr[flag] = max(max(ans1,ans2),ans3);
-    }
-    next = curr;
-    }
-    return curr[0];
-
-} 
-
 
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        //recursion
-     // return   solveRec(prices,0,false);
-
-     //memoization
+        //RECURSION 
+ //return solveRec(0,false,prices);
+     
+     //MEMOIZATION 
      vector<vector<int>>dp(n+1,vector<int>(2,-1));
-     //return solveMemo(prices,0,false,dp);
-
-     //tabulation
-     //return solveTab(prices);
-
-     //space optimization
-     return solveSO(prices);
-
-
+     return solveMemo(0,0,prices,dp);
+        
     }
 };
