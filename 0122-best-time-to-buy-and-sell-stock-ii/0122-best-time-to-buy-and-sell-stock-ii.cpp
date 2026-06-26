c43258344580798b907b0abebd07a5f873a8d6c4
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-26 10:22:12 +0530

    Time: 0 ms (100%), Space: 20.1 MB (63.57%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -48,14 +48,82 @@ int solveMemo(int i , int flag ,vector<int>& prices, vector<vector<int>>&dp ){
     return dp[i][flag] = ans;
 }
 
+// TABULATION
+
+int solveTab(vector<int>& prices){
+    int n = prices.size();
+    vector<vector<int>>dp(n+1,vector<int>(2,0));
+
+
+    for(int i=n-1;i>=0;i--){
+        for(int flag=1;flag>=0;flag--){
+            int ans = 0;
+
+        if(flag==0){
+         int  ans1  =   - prices[i]+ dp[i+1][1];
+         int ans2 =     dp[i+1][flag];
+          ans =   max(ans1,ans2) ;
+        }
+
+        else {
+       int  ans1  = prices[i] + dp[i+1][0];
+       int   ans2  = dp[i+1][flag];
+       ans  =   max(ans1,ans2);
+    }  
+      dp[i][flag] = ans;
+
+
+        }
+    }
+    return dp[0][0];
+}
+// SPACE OPTIMIZATION .
+
+int solveSO(vector<int>& prices){
+    int n = prices.size();
+   vector<int>curr(2,0);
+   vector<int>next(2,0);
+
+
+    for(int i=n-1;i>=0;i--){
+        for(int flag=1;flag>=0;flag--){
+            int ans = 0;
+
+        if(flag==0){
+         int  ans1  =   - prices[i]+ next[1];
+         int ans2 =     next[flag];
+          ans =   max(ans1,ans2) ;
+        }
+
+        else {
+       int  ans1  = prices[i] + next[0];
+       int   ans2  = next[flag];
+       ans  =   max(ans1,ans2);
+    }  
+      curr[flag] = ans;
+
+
+        }
+        next = curr;
+    }
+    return next[0];
+}
+
+
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
         //RECURSION 
  //return solveRec(0,false,prices);
      
      //MEMOIZATION 
-     vector<vector<int>>dp(n+1,vector<int>(2,-1));
-     return solveMemo(0,0,prices,dp);
+    //  vector<vector<int>>dp(n+1,vector<int>(2,-1));
+    //  return solveMemo(0,0,prices,dp);
         
+     // TABULATION 
+     //return solveTab(prices);
+    
+
+    //SPACE OPTIMIZATION 
+    return solveSO(prices);
     }
 };
