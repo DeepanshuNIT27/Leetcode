4afaa76766270f6bd88d1ac0e0116f5ca042ec68
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-26 12:20:27 +0530

    Time: 75 ms (60.16%), Space: 79.4 MB (71.25%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -51,6 +51,72 @@ int solveMemo(int i , int flag , int trans , vector<int>& prices, vector<vector<
     return dp[i][flag][trans] = ans;
 }
 
+//TABULATION 
+int solveTab(vector<int>& prices){
+     int  n = prices.size();
+      vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
+
+      for(int i=n-1;i>=0;i--){
+        for(int flag=1;flag>=0;flag--){
+            for(int trans=2;trans>0;trans--){
+                int ans = 0;
+            if(flag ==0 ) {
+
+        int ans1 = -prices[i] + dp[i+1][1][trans];
+        int ans2 = dp[i+1][flag][trans];
+
+        ans = max(ans1,ans2);
+        }
+         else {
+
+        int ans1 = prices[i] + dp[i+1][0][trans-1];
+        int ans2 = dp[i+1][flag][trans];
+
+        ans = max(ans1,ans2);
+    }
+    dp[i][flag][trans] = ans;
+            }
+        }
+      
+      }
+      return dp[0][0][2];
+
+}
+//SPACE OPTIMIZATION 
+
+int solveSO(vector<int>& prices){
+     int  n = prices.size();
+      vector<vector<vector<int>>>dp(2,vector<vector<int>>(2,vector<int>(3,0)));
+
+      for(int i=n-1;i>=0;i--){
+        for(int flag=1;flag>=0;flag--){
+            for(int trans=2;trans>0;trans--){
+                int ans = 0;
+            if(flag ==0 ) {
+
+        int ans1 = -prices[i] + dp[1][1][trans];
+        int ans2 = dp[1][flag][trans];
+
+        ans = max(ans1,ans2);
+        }
+         else {
+
+        int ans1 = prices[i] + dp[1][0][trans-1];
+        int ans2 = dp[1][flag][trans];
+
+        ans = max(ans1,ans2);
+    }
+    dp[0][flag][trans] = ans;
+            }
+        }
+        dp[1] = dp[0];
+      
+      }
+      return dp[0][0][2];
+
+}
+
+
 
     int maxProfit(vector<int>& prices) {
         int  n = prices.size();
@@ -59,9 +125,15 @@ int solveMemo(int i , int flag , int trans , vector<int>& prices, vector<vector<
         //return solveRec(0,0,2,prices);
       
       //MEMOIZATION 
-        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
+      //  vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,-1)));
 
-        return solveMemo(0,0,2,prices,dp);
+       // return solveMemo(0,0,2,prices,dp);
+
+       //TABULATION 
+       //return solveTab(prices);
+
+       // SPACE OPTIMIZATION 
+       return solveSO(prices);
 
 
     }
