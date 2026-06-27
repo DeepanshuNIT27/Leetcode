3bcc0c8d06858433d2377965794cce8ef5a8e804
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-27 10:26:52 +0530

    Time: 586 ms (8.89%), Space: 296.7 MB (6.38%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,15 +1,45 @@
 class Solution {
 public:
+// RECURSION 
+int solveRec(int i , int prev ,vector<int>& nums ){
+
+    if(i>= nums.size()) return 0;
+    
+    int ans = 0;
+    if(prev == -1 || nums[prev] < nums[i]){
+        ans =  1 + solveRec(i+1,i,nums);
+    }
+
+    int ans2 = solveRec(i+1,prev,nums);
+
+    return max(ans,ans2);
+}
+
+//MEMOIZATION 
+
+int solveMemo(int i , int prev ,vector<int>& nums, vector<vector<int>>&dp ){
+
+    if(i>= nums.size()) return 0;
+    if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    
+    int ans = 0;
+    if(prev == -1 || nums[prev] < nums[i]){
+        ans =  1 + solveMemo(i+1,i,nums,dp);
+    }
+
+    int ans2 = solveMemo(i+1,prev,nums,dp);
+
+    return  dp[i][prev+1] =  max(ans,ans2);
+}
+
+
     int lengthOfLIS(vector<int>& nums) {
-        vector<int>dp;
-        dp.push_back(nums[0]);
-        for(int i=1;i<nums.size();i++){
-          if(nums[i]>dp.back()) dp.push_back(nums[i]);
-          else{
-            auto it = lower_bound(dp.begin(),dp.end(),nums[i]) - dp.begin();
-            dp[it] = nums[i];
-          }
-        }
-        return dp.size();
+       int n = nums.size();
+        //RECURSION 
+       // return solveRec(0,-1,nums);
+
+       //MEMOIZATION 
+       vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+       return solveMemo(0,-1,nums,dp);
     }
 };
