111e60b0c860f368be3bba7c44fa0faa12801645
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-06-30 10:15:23 +0530

    Time: 141 ms (16.94%), Space: 89.4 MB (53.53%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -31,6 +31,31 @@ int solveMemo(int i , int j , vector<int>& nums1, vector<int>& nums2, vector<vec
     return  dp[i][j] = ans;
 }
 
+//TABULATION 
+int solveTab(vector<int>& nums1, vector<int>& nums2){
+         int n  = nums1.size();
+         int m = nums2.size();
+     vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+       
+       int final = 0;
+     for(int i=n-1;i>=0;i--){
+        for(int j=m-1;j>=0;j--){
+
+        int ans = 0;
+         if(nums1[i] == nums2[j]){
+         ans  = 1 + solveMemo(i+1,j+1,nums1,nums2,dp);
+          }
+        else {
+        ans  = 0;
+         }
+         dp[i][j] = ans;
+         final = max(final,dp[i][j]);
+        }
+     }
+     return final;
+
+}
+
 
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         int n  = nums1.size();
@@ -38,14 +63,18 @@ int solveMemo(int i , int j , vector<int>& nums1, vector<int>& nums2, vector<vec
 
         //RECURSION 
         //MEMOIZATION 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        int ans  = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-               // ans  = max(ans,solveRec(i,j,nums1,nums2));
-                ans  = max(ans,solveMemo(i,j,nums1,nums2,dp));
-            }
-        }
-        return ans;
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // int ans  = 0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //        // ans  = max(ans,solveRec(i,j,nums1,nums2));
+        //         ans  = max(ans,solveMemo(i,j,nums1,nums2,dp));
+        //     }
+        // }
+        //return ans;
+
+       // TABULATION
+       return solveTab(nums1,nums2); 
+
     }
 };
