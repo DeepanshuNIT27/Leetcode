780fbb810964b90672c5475fa5d2f474487b6977
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-07-01 16:48:10 +0530

    Time: 147 ms (8.02%), Space: 55.1 MB (5.83%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,153 +1,84 @@
 class Solution {
 public:
-//recursive soln
-  bool solveRec(string &s, string &p , int i, int j){
-    if(i== s.length() && j== p.length()){
-        //dono bahar nikl gye
-        return true;
-    }
-    if(j == p.length()){
-        //pattern khtm ho gya but string ni
-        return false;
-    }
-    if( i == s.length()){
-        for(int k=j ;k<p.length();k++){
-            if(p[k] !='*'){
-            return false;
-            }
+//RECURSION 
+bool solveRec(int i, int j , string &s, string &p){
+
+    //BASE CASE 
+    if(i == s.size()){
+        while(j<p.size()){
+            if(p[j]!='*') return false;
+            j++;
         }
         return true;
     }
-    if(p[j] =='?' || s[i]==p[j]){
-       return solveRec(s,p,i+1,j+1);
+    if(j == p.size()){
+       if(i != s.size()) return false;
+       return true;
     }
-    else if( p[j]=='*'){
-     return solveRec(s,p,i+1,j) || solveRec(s,p,i,j+1);
+
+
+    // MATCH AND '?' CASE
+    bool ans = false;
+    if(s[i] == p[j] || p[j] =='?'){
+         ans  =  ans || solveRec(i+1,j+1,s,p);
     }
-    else{
-        return false;
-    }
-  return  false;
-  }
-  //memorization
-   bool solveMemo(string &s, string &p , int i, int j,vector<vector<int>>&dp){
-    if(i== s.length() && j== p.length()){
-        //dono bahar nikl gye
-        return true;
-    }
-    if(j == p.length()){
-        //pattern khtm ho gya but string ni
-        return false;
-    }
-    if( i == s.length()){
-        for(int k=j ;k<p.length();k++){
-            if(p[k] !='*'){
-            return false;
-            }
+    else {
+        if(p[j] == '*'){
+            ans  = ans ||  solveRec(i+1,j,s,p);
+            ans = ans || solveRec(i,j+1,s,p);
         }
-        return true;
+        else {
+            return false;
+        }
+    }
+    return ans;
+}
+
+//MEMOIZATION 
+bool solveMemo(int i, int j , string &s, string &p, vector<vector<int>>&dp){
+
+    //BASE CASE 
+    if(i == s.size() && j==p.size()) return dp[i][j] = true;
+    if(i == s.size()){
+        while(j<p.size()){
+            if(p[j]!='*') return dp[i][j] =  false;
+            j++;
+        }
+        return  dp[i][j] = true;
+    }
+    if(j == p.size()){
+       return dp[i][j] = false;
     }
     if(dp[i][j]!=-1) return dp[i][j];
 
-     bool ans ;
-    if(p[j] =='?' || s[i]==p[j]){
-       ans =  solveMemo(s,p,i+1,j+1,dp);
+
+    // MATCH AND '?' CASE
+    bool ans = false;
+    if(s[i] == p[j] || p[j] =='?'){
+         ans  =  ans || solveMemo(i+1,j+1,s,p,dp);
     }
-    else if( p[j]=='*'){
-     ans =  solveMemo(s,p,i+1,j,dp) || solveMemo(s,p,i,j+1,dp);
-    }
-    else{
-        ans = false;
-    }
-     dp[i][j] = ans;   
-  return  dp[i][j];
-  }
-  //tabulation
-  bool solveTab(string s, string p, int m,int n){
-    vector<vector<int>>dp(m+1,vector<int>(n+1,0));
-    dp[m][n] = true;
-   
-   for(int col =0 ;col<n ;col++){
-    bool flag = true;
-        for(int k=col ;k<p.length();k++){
-            if(p[k] !='*'){
-            flag = false;
-            break;
-            }
+    else {
+        if(p[j] == '*'){
+           bool  ans1  = solveMemo(i+1,j,s,p,dp);
+           bool ans2 =  solveMemo(i,j+1,s,p,dp);
+
+           ans  = ans1 || ans2;
         }
-         dp[m][col] = flag;
-    }
-
-
-   for(int i=m-1;i>=0;i--){
-    for(int j=n-1 ;j>=0 ;j--){
-     bool ans ;
-     if(p[j] =='?' || s[i]==p[j]){
-       ans =  dp[i+1][j+1];
-    }
-    else if( p[j]=='*'){
-     ans =  dp[i+1][j] || dp[i][j+1];
-    }
-    else{
-        ans = false;
-    }
-     dp[i][j] = ans;   
-    }
-   }
-  return  dp[0][0];
-  }
-//space opti
-bool solveTabSO(string s, string p, int m,int n){
-   vector<int>next(n+1,0);
-   vector<int>curr(n+1,0);
-    next[n] = true;
-   
-   for(int col =0 ;col<n ;col++){
-    bool flag = true;
-        for(int k=col ;k<p.length();k++){
-            if(p[k] !='*'){
-            flag = false;
-            break;
-            }
+        else {
+            return  dp[i][j] = false;
         }
-         next[col] = flag;
     }
-
-
-   for(int i=m-1;i>=0;i--){
-    for(int j=n-1 ;j>=0 ;j--){
-     bool ans ;
-     if(p[j] =='?' || s[i]==p[j]){
-       ans =  next[j+1];
-    }
-    else if( p[j]=='*'){
-     ans =  next[j] || curr[j+1];
-    }
-    else{
-        ans = false;
-    }
-     next[j] = ans;   
-    }
-    curr = next;
-   }
-  return  next[0];
-  }
+    return  dp[i][j] =  ans;
+}
 
     bool isMatch(string s, string p) {
-        int m = s.size();
-        int n = p.size();
-        //recursive
-       // return solveRec(s,p,0,0);
+        int n = s.size();
+        int m = p.size();
+        //RECURSION 
+        //return solveRec(0,0,s,p);
 
-       //memorization
-    //    vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-    //    return solveMemo(s,p,0,0,dp);
-
-       //tabulation 
-      return  solveTab(s,p,m,n);
-
-      //space opti
-      return solveTabSO(s,p,m,n);
-        
+        //MEMOIZATION 
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return solveMemo(0,0,s,p,dp);
     }
 };
