716eec5f3586565b1e15fcac9a5128775349a1c8
DeepanshuNIT27 <169021074+DeepanshuNIT27@users.noreply.github.com>
2026-07-10 11:03:41 +0530

    Time: 38 ms (32.81%), Space: 14 MB (53.4%) - LeetHub

---
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+//RECURSION 
+int solveRec(int i , int j ,vector<int>& cuts ){
+
+    if( i+1 == j) return 0;
+
+    int mini = 1e9;
+    for(int k=i+1;k<j;k++){ 
+       int step = cuts[j] - cuts[i] + solveRec(i,k,cuts) + solveRec(k,j,cuts);
+       mini = min(mini,step);
+    }
+    return mini;
+}
+//MEMOIZATION 
+int solveMemo(int i , int j ,vector<int>& cuts, vector<vector<int>>&dp ){
+
+    if( i+1 == j) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+
+    int mini = 1e9;
+    for(int k=i+1;k<j;k++){ 
+       int step = cuts[j] - cuts[i] + solveMemo(i,k,cuts,dp) + solveMemo(k,j,cuts,dp);
+       mini = min(mini,step);
+    }
+    return  dp[i][j] = mini;
+}
+    int minCost(int n, vector<int>& cuts) {
+       
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(),cuts.end());
+         //RECURSION 
+        //return solveRec(0,cuts.size()-1,cuts);
+
+        //MEMOIZATION 
+        int m = cuts.size();
+        vector<vector<int>>dp(m+1,vector<int>(m+1,-1));
+        return solveMemo(0,m-1,cuts,dp);
+
+    }
+};
